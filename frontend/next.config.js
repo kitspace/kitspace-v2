ea92122bb4be32ba8db1ffe80efcc205e5821f10
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-12-26 06:48:24 +0000

    Try next-fonts loader

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,27 +1,30 @@
 const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
+const withFonts = require('next-fonts')
 
 module.exports = withSass(
-  withCss({
-    webpack(config) {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty',
-      }
-      config.module.rules.push({
-        test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            publicPath: './',
-            outputPath: 'static/',
-            name: '[name].[ext]',
+  withFonts(
+    withCss({
+      webpack(config) {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+          fs: 'empty',
+        }
+        config.module.rules.push({
+          test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              publicPath: './',
+              outputPath: 'static/',
+              name: '[name].[ext]',
+            },
           },
-        },
-      })
+        })
 
-      return config
-    },
-  }),
+        return config
+      },
+    }),
+  ),
 )
