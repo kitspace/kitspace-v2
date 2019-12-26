b205bb0ec4dfa565898409b75b218fcfa51fdc81
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-12-26 18:01:06 +0000

    Make semantic-ui icons work

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,30 +1,26 @@
 const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
-const withFonts = require('next-fonts')
 
 module.exports = withSass(
-  withFonts(
-    withCss({
-      webpack(config) {
-        // Fixes npm packages that depend on `fs` module
-        config.node = {
-          fs: 'empty',
-        }
-        config.module.rules.push({
-          test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
-              publicPath: './',
-              outputPath: 'static/',
-              name: '[name].[ext]',
-            },
+  withCss({
+    webpack(config) {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty',
+      }
+      config.module.rules.push({
+        test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/',
+            outputPath: 'static',
+            name: '[name].[hash].[ext]',
           },
-        })
+        },
+      })
 
-        return config
-      },
-    }),
-  ),
+      return config
+    },
+  }),
 )
