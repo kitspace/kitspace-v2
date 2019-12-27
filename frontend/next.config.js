746c0ebfff3293b97eda9ade2dd3d195a210c035
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-12-27 16:59:36 +0000

    Start implementing static error pages

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,7 +16,7 @@ module.exports = withSass(
         use: {
           loader: 'file-loader',
           options: {
-            publicPath: '/_next/static/',
+            publicPath: '/error_pages/_next/static/',
             outputPath: 'static',
             name: '[name].[hash].[ext]',
           },
@@ -24,6 +24,56 @@ module.exports = withSass(
       })
 
       return config
+    },
+    assetPrefix: '/error_pages',
+    exportPathMap() {
+      const codes = [
+        400,
+        401,
+        402,
+        403,
+        404,
+        405,
+        406,
+        407,
+        408,
+        409,
+        410,
+        411,
+        412,
+        413,
+        414,
+        415,
+        416,
+        417,
+        418,
+        421,
+        422,
+        423,
+        424,
+        425,
+        426,
+        428,
+        429,
+        431,
+        451,
+        500,
+        501,
+        502,
+        503,
+        504,
+        505,
+        506,
+        507,
+        508,
+        510,
+        511,
+      ]
+      const map = {}
+      codes.forEach(err => {
+        map[err] = { page: '/_error', query: { staticStatusCode: err } }
+      })
+      return map
     },
   }),
 )
--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Error from 'next/error'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import TitleBar from '../components/TitleBar'
 
@@ -13,8 +14,14 @@ const KitspaceError = ({ statusCode }) => {
   )
 }
 
-KitspaceError.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+KitspaceError.getInitialProps = ({ res, err, query }) => {
+  const statusCode = query.staticStatusCode
+    ? query.staticStatusCode
+    : res
+    ? res.statusCode
+    : err
+    ? err.statusCode
+    : 404
   return { statusCode }
 }
 
