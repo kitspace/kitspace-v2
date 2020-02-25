04504559b9fa43d00a2013fe128e198a2d801277
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-25 19:46:29 +0000

    Try getting a post request to /repos/migrate

--- frontend/pages/projects/new.js
+++ frontend/pages/projects/new.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 import superagent from 'superagent'
-import { Grid, Divider, Form, Input } from 'semantic-ui-react'
+import { Grid, Divider, Input } from 'semantic-ui-react'
 import 'semantic-ui-css/components/grid.css'
 import 'semantic-ui-css/components/divider.css'
-import 'semantic-ui-css/components/form.css'
 import 'semantic-ui-css/components/input.css'
+import path from 'path'
 
 import Head from '../../components/Head'
 import TitleBar from '../../components/TitleBar'
@@ -16,7 +16,10 @@ const gitea_public_url = `${process.env.KITSPACE_GITEA_URL}/api/v1`
 
 const gitea_internal_url = 'http://gitea:3000/api/v1'
 
-function New({ name, _csrf }) {
+function New({ user, _csrf }) {
+  const [remoteRepo, setRemoteRepo] = React.useState('')
+  const remoteRepoPlaceHolder = 'https://github.com/emard/ulx3s'
+  const uid = user.id
   return (
     <>
       <Head />
@@ -30,13 +33,33 @@ function New({ name, _csrf }) {
                 action={{
                   content: 'Sync',
                   color: 'green',
-                  onClick: e => console.log(e),
+                  onClick: e => {
+                    const clone_addr = remoteRepo || remoteRepoPlaceHolder
+                    const repo_name = urlToName(clone_addr)
+                    //fetch(gitea_public_url + '/repos/migrate', {
+                    //  method: 'POST',
+                    //  credentials: 'include',
+                    //  headers: {
+                    //    'Content-Type': 'application/json',
+                    //    Accept: 'application/json',
+                    //  },
+                    //  body: JSON.stringify({ _csrf, clone_addr, repo_name, uid }),
+                    //  mode: 'no-cors',
+                    //}).then(r => console.log(r))
+                    superagent
+                      .post('http://gitea.kitspace.test:3000/api/repo/migrate')
+                      .withCredentials()
+                      .query({ _csrf, clone_addr, repo_name, uid })
+                      .then(r => {
+                        console.log({ r })
+                      })
+                  },
                 }}
                 className="urlInput"
                 fluid
-                onChange={e => console.log(e)}
-                placeholder="https://github.com/emard/ulx3s"
-                value=""
+                onChange={e => setRemoteRepo(e.target.value)}
+                placeholder={remoteRepoPlaceHolder}
+                value={remoteRepo}
               />
             </div>
           </Grid.Column>
@@ -77,14 +100,18 @@ function getSession(req) {
   return session
 }
 
+function urlToName(url) {
+  url = new URL(url)
+  return path.basename(url.pathname, path.extname(url.pathname))
+}
+
 New.getInitialProps = async ({ req, query }) => {
-  const api = req ? gitea_internal_url : gitea_public_url
   const session = getSession(req)
   const cookie = req?.headers?.cookie
   const _csrf = session._csrf
 
   return {
-    name: session.user?.username || 'unknown user',
+    user: session.user,
     _csrf,
   }
 }
