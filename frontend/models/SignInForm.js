cf0bab78389353991e5d477aade6a365605d22d9
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2020-08-28 13:46:32 +0200

    Update validation model for sign-in form.

--- frontend/models/SignInForm.js
+++ frontend/models/SignInForm.js
@@ -1,9 +1,6 @@
 import Joi from 'joi'
 
-export const SignUpForm = Joi.object({
+export const SignInForm = Joi.object({
   username: Joi.string().alphanum().required(),
-  password: Joi.string()
-    .pattern(new RegExp('^[a-zA-Z0-9!@#%^&*()_]'))
-    .min(6)
-    .required(),
+  password: Joi.string().required(),
 })
