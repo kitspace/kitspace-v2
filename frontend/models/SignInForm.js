2358efe7c1bed7ce0ebe589ad613c965c8b29469
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2020-08-28 10:08:23 +0200

    Add validation models.

---
+++ frontend/models/SignInForm.js
@@ -0,0 +1,8 @@
+import Joi from 'joi'
+
+export const SignUpForm = Joi.object({
+  username: Joi.string().alphanum().required(),
+  password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9!@#%^&*()_]'))
+    .min(6).required(),
+})
---
+++ frontend/models/SignUpForm.js
@@ -0,0 +1,10 @@
+import Joi from 'joi'
+
+export const SignUpForm = Joi.object({
+  username: Joi.string().alphanum().required(),
+  email: Joi.string().email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string()
+    .pattern(new RegExp('^[a-zA-Z0-9]'))
+    .min(6).required(),
+})
