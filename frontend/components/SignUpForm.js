16d6249f42e62eba16b427b41427a289d50a9bd5
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2020-08-28 11:47:21 +0200

    Add client side validation to sign-up form.

--- frontend/components/SignUpForm.js
+++ frontend/components/SignUpForm.js
@@ -1,41 +1,74 @@
 import React from 'react'
-import {Button, Form, Header, Segment} from 'semantic-ui-react'
+import { Button, Form, Header, Segment, Input, Message } from 'semantic-ui-react'
 
 import ExternalAuthProviders from './ExternalAuthProviders'
+import useForm from '../hooks/useForm'
+import { SignUpForm } from '../models/SignUpForm'
 
-export default function () {
+
+export default function()  {
+  const [form, onChange, isValid, errors] = useForm(SignUpForm)
+
+  const submit = () => {
+    console.log(errors)
+  }
+
   return (
     <>
       <Header as="h2" textAlign="center">
         Create a new account
       </Header>
+      <Message
+        negative
+        style={{ display: form[errors.field] === undefined ? 'none' : 'block' }}
+      >
+        {errors.msg}
+      </Message>
       <Form size="large">
         <Segment stacked>
-          <Form.Input
+          <Input
             fluid
             icon="user"
             iconPosition="left"
             placeholder="username"
-            style={{marginBottom: 20}}
+            name="username"
+            value={form.username || ''}
+            onChange={onChange}
+            error={errors.field === 'username' && form.username !== undefined}
+            style={{ marginBottom: 20 }}
           />
         </Segment>
         <Segment stacked>
-          <Form.Input
+          <Input
             fluid
             icon="mail"
             iconPosition="left"
             placeholder="email"
-            style={{marginBottom: 20}}
+            name="email"
+            value={form.email || ''}
+            error={errors.field === 'email' && form.email !== undefined}
+            onChange={onChange}
+            style={{ marginBottom: 20 }}
           />
-          <Form.Input
+          <Input
             fluid
             icon="lock"
             iconPosition="left"
             placeholder="Password"
             type="password"
-            style={{marginBottom: 20}}
+            name="password"
+            value={form.password || ''}
+            onChange={onChange}
+            error={errors.field === 'password' && form.password !== undefined}
+            style={{ marginBottom: 20 }}
           />
-          <Button color="green" fluid size="large">
+          <Button
+            fluid
+            color="green"
+            size="large"
+            onClick={submit}
+            disabled={!isValid}
+          >
             Sign up
           </Button>
         </Segment>
