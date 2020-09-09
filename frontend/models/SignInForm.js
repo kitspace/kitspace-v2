40af9fff10fdadc4aaf547ddbb2d9ec494339b2a
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2020-09-09 11:15:54 +0200

    Update sign in form constrains.

--- frontend/models/SignInForm.js
+++ frontend/models/SignInForm.js
@@ -1,6 +1,6 @@
 import Joi from 'joi'
 
 export const SignInForm = Joi.object({
-  username: Joi.string().alphanum().required(),
+  username: Joi.string().alphanum().required().max(20),
   password: Joi.string().required(),
 })
