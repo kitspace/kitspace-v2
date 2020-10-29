12d03f878e01ee5a4480eb3d70daebf105c1fee6
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2020-10-29 15:54:50 +0200

    Add propTypes to `Product` component.

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,22 +1,32 @@
 import React from 'react'
+import { string, number } from 'prop-types'
+
 import { Button, Divider, Grid, Image, Segment } from 'semantic-ui-react'
 
+const Product = ({ name, img_uri, price, description }) => {
+  return (
+    <Segment>
+      <Grid columns={2}>
+        <Grid.Column>
+          <Image src={img_uri} fluid />
+        </Grid.Column>
+        <Grid.Column>
+          <h2 style={{textTransform: 'capitalize', fontSize: '4rem'}}>{name}</h2>
+          <p>{price}</p>
+          <p>{description}</p>
+          <Button />
+        </Grid.Column>
+      </Grid>
+      <Divider vertical />
+    </Segment>
+  )
+}
 
-const Product = ({name, img_uri, price, description}) => {
-  return <Segment>
-    <Grid columns={2}>
-      <Grid.Column>
-        <Image  src={img_uri} fluid/>
-      </Grid.Column>
-      <Grid.Column>
-        <h2>{name}</h2>
-        <p>{price}</p>
-        <p>{description}</p>
-        <Button />
-      </Grid.Column>
-    </Grid>
-    <Divider vertical />
-  </Segment>
+Product.propTypes = {
+  name: string,
+  img_uri: string,
+  price: number,
+  description: string,
 }
 
 export default Product
