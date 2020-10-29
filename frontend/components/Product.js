10949910114c08c7bdf87aa0859c4aaeefa7fc45
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2020-10-29 18:26:46 +0200

    Add basic Skeleton to `Product`.

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import { string, number } from 'prop-types'
 
-import { Button, Divider, Grid, Image, Segment } from 'semantic-ui-react'
+import { Button, Grid, Image, Segment } from 'semantic-ui-react'
 
-const Product = ({ name, img_uri, price, description }) => {
+
+const ProductImage = ({ src }) => {
+  return <Image src={src} fluid />
+}
+
+const Product = ({ name, img_uri, price, description, projectLink }) => {
   return (
-    <Segment>
+    <Segment style={{ border: 'none', boxShadow: 'none' }}>
       <Grid columns={2}>
-        <Grid.Column>
-          <Image src={img_uri} fluid />
+        <Grid.Column style={{ borderRight: 'solid #B0BAA3 1px' }}>
+          <ProductImage src={img_uri} />
         </Grid.Column>
-        <Grid.Column>
-          <h2 style={{textTransform: 'capitalize', fontSize: '4rem'}}>{name}</h2>
+        <Grid.Column style={{paddingLeft: '3rem'}}>
+          <h2 style={{ textTransform: 'capitalize' }}>{name}</h2>
           <p>{price}</p>
           <p>{description}</p>
+          <p>
+            See complete project details{' '}
+            <a href={projectLink} target="_blank">
+              here.
+            </a>
+          </p>
           <Button />
         </Grid.Column>
       </Grid>
-      <Divider vertical />
     </Segment>
   )
 }
@@ -27,6 +37,7 @@ Product.propTypes = {
   img_uri: string,
   price: number,
   description: string,
+  projectLink: string,
 }
 
 export default Product
