abf6fff096c422406bee1007f99c3c3fe3e9a7e6
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2020-10-30 11:34:56 +0200

    Refactor `buy` page to use the `product` component.

--- frontend/pages/buy/success.js
+++ frontend/pages/buy/success.js
@@ -1,58 +1,23 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
-import Head from '../../components/Head'
-import TitleBar from '../../components/TitleBar'
 
+import { Page } from '../../components/Page'
+import styles from './styles.module.scss'
 
-export default function SuccessPage({ user, _csrf }) {
-  const uid = user?.id
+export default function SuccessPage() {
   return (
-    <>
-      <Head />
-      <TitleBar route="/buy/success" />
-      <Container style={{ marginTop: 50, width: '100%' }}>
-        <div
-          style={{
-            marginTop: 400,
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
+      <Page>
+        <div className={styles.wrapper}
         >
-          <div style={{ fontSize: '24px' }}>Thank you for your order.</div>
-          <div style={{ marginTop: 20 }}>
+          <div className={styles.message}>Thank you for your order.</div>
+          <div className={styles.retry}>
             <a
-              style={{ fontWeight: 'normal', color: '#555555' }}
+              className={styles.link}
               href="https://kitspace.org"
             >
               browse projects
             </a>
           </div>
         </div>
-      </Container>
-    </>
+      </Page>
   )
-}
-
-function getSession(req) {
-  let session = {}
-  if (req != null && req.session) {
-    session = req.session
-  } else if (typeof window !== 'undefined' && window.session) {
-    session = window.session
-  }
-  return session
-}
-
-SuccessPage.getInitialProps = async ({ req, query }) => {
-  const session = getSession(req)
-  const cookie = req?.headers?.cookie
-  const _csrf = session._csrf
-
-  return {
-    user: session.user,
-    _csrf,
-  }
 }
