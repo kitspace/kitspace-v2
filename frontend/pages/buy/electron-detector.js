1c492cc8d06c96e0ffe601ee4d4c5a75b33f979f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-12-10 12:18:05 +0000

    Add more description to electron detector kit

--- frontend/pages/buy/electron-detector.js
+++ frontend/pages/buy/electron-detector.js
@@ -17,7 +17,10 @@ const BuyPage = () => {
               photons). It's an educational tool and citizen science device made for
               exploring natural and synthetic sources of radioactivity such as
               stones, airborne radon, potassium-rich salt or food and every-day
-              objects (Uranium glass, old Radium watches etc.). See full project
+              objects (Uranium glass, old Radium watches etc.).
+            </p>
+            <p>
+              This project is developed by Oliver Keller at CERN, see full project
               details <a href={projectLink}>here.</a>
             </p>
             <p>
@@ -34,7 +37,18 @@ const BuyPage = () => {
         price={3000}
         priceId="price_1Hi40TI6rpeFFqzwPHRltaPl"
         shippingPrice={1500}
-        shippingName="Worldwide Shipping"
+        shippingName={
+          'Worldwide Shipping (estimated delivery by ' +
+          new Date(new Date().getTime() + 10 * 86400000).toLocaleDateString(
+            undefined,
+            {
+              weekday: 'long',
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            },
+          ) + ")"
+        }
         shippingPriceId="price_1Hi40LI6rpeFFqzwKKdjjpeK"
       />
     </Page>
