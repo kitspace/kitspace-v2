96aa167bbf6d801e8f2f8488d5b9c4966c29da39
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-12-31 15:26:23 +0000

    Add some processor integration tests

--- processor/test/integration/test_app.js
+++ processor/test/integration/test_app.js
@@ -1,12 +1,26 @@
-const request = require('supertest')
+const supertest = require('supertest')
 const assert = require('assert')
 
 const { createApp } = require('../../src/app')
 
 describe('app', () => {
+  beforeEach(() => {
+    this.app = createApp('/tmp/repos')
+    this.supertest = supertest(this.app)
+  })
   it('creates app', () => {
-    const app = createApp()
-    assert(app != null)
-    app.stop()
+    assert(this.app != null)
+  })
+  it('404s root', () => {
+    return this.supertest.get('/').expect(404)
+  })
+  it('404s a file', () => {
+    return this.supertest.get('/files/user/project/HEAD/top.png').expect(404)
+  })
+  it('404s status', () => {
+    return this.supertest.get('/files/user/project/HEAD/top.png').expect(404)
+  })
+  afterEach(() => {
+    this.app.stop()
   })
 })
