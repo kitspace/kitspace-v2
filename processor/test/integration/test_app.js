db5a41e4f52379cdd908006c01214cbd3687e8e2
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-12-31 17:40:25 +0000

    Write a proper integration test for processor

--- processor/test/integration/test_app.js
+++ processor/test/integration/test_app.js
@@ -1,26 +1,65 @@
 const supertest = require('supertest')
 const assert = require('assert')
+const cp = require('child_process')
+const util = require('util')
+const path = require('path')
+
+const exec = util.promisify(cp.exec)
 
 const { createApp } = require('../../src/app')
 
+const tmpDir = '/tmp/kitspace-processor-test'
+const repoDir = path.join(tmpDir, 'repos')
+const sourceRepo = path.join(tmpDir, 'source-repo')
+process.env.DATA_DIR = path.join(tmpDir, 'data')
+
 describe('app', () => {
-  beforeEach(() => {
-    this.app = createApp('/tmp/repos')
+  beforeEach(async () => {
+    await exec(`mkdir -p ${repoDir}`)
+    this.app = createApp(repoDir)
     this.supertest = supertest(this.app)
   })
-  it('creates app', () => {
+  it('creates app', async () => {
     assert(this.app != null)
   })
-  it('404s root', () => {
-    return this.supertest.get('/').expect(404)
+  it('404s root', async () => {
+    await this.supertest.get('/').expect(404)
   })
-  it('404s a file', () => {
-    return this.supertest.get('/files/user/project/HEAD/top.png').expect(404)
+  it('404s a file', async () => {
+    await this.supertest.get('/files/user/project/HEAD/top.png').expect(404)
   })
-  it('404s status', () => {
-    return this.supertest.get('/files/user/project/HEAD/top.png').expect(404)
+  it('404s status', async () => {
+    await this.supertest.get('/files/user/project/HEAD/top.png').expect(404)
   })
-  afterEach(() => {
+  it('reports status of failed processing', async () => {
+    // make a simple git repo without the right files
+    await exec(`mkdir -p ${sourceRepo}`)
+    await exec(
+      `cd ${sourceRepo} && git init && touch test-file && git add test-file && git commit -m 'Initial commit'`,
+    )
+    // gitea uses bare repos, so we clone it into one
+    await exec(
+      `git clone --bare ${sourceRepo} ${path.join(repoDir, 'user/project.git')}`,
+    )
+    // clean up
+    await exec(`rm -rf ${sourceRepo}`)
+    let r = await this.supertest.get('/status/user/project/HEAD/images/top.png')
+    // at first it may not be processing yet so we get a 404
+    while (r.status === 404) {
+      r = await this.supertest.get('/status/user/project/HEAD/images/top.png')
+    }
+    // after a while the status should be ready
+    assert(r.status === 200)
+    // but it's probably 'in_progress'
+    while (r.body.status === 'in_progress') {
+      r = await this.supertest.get('/status/user/project/HEAD/images/top.png')
+    }
+    // at some point it should notice it failed
+    assert(r.status === 200)
+    assert(r.body.status === 'failed')
+  })
+  afterEach(async () => {
     this.app.stop()
+    await exec(`rm -rf ${repoDir}`)
   })
 })
