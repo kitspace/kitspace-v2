5f5266da02ce59b474d24189c4a1c83f304dce4a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-01-01 23:00:37 +0000

    Prevent concurrently trying to process the same project

--- processor/src/watcher.js
+++ processor/src/watcher.js
@@ -16,6 +16,7 @@ const processIBOM = require('./tasks/processIBOM')
 const exec = util.promisify(cp.exec)
 const readFile = util.promisify(fs.readFile)
 
+const running = {}
 function watch(eventEmitter, repoDir = '/repositories') {
   let dirWatchers = {}
 
@@ -23,7 +24,15 @@ function watch(eventEmitter, repoDir = '/repositories') {
   const handleAddDir = gitDir => {
     console.info('addDir', gitDir)
     // we debounce the file-system event to only invoke once per change in the repo
-    const debouncedRun = debounce(() => run(eventEmitter, repoDir, gitDir), 1000)
+    // additionally we ignore any invocations that happen while it's already running
+    // to prevent it from trying to overwrite files that are already being written to
+    const debouncedRun = debounce(async () => {
+      if (!running[gitDir]) {
+        running[gitDir] = true
+        await run(eventEmitter, repoDir, gitDir)
+        running[gitDir] = false
+      }
+    }, 1000)
     dirWatchers[gitDir] = {}
     dirWatchers[gitDir].add = chokidar.watch(gitDir).on('add', debouncedRun)
     // if the repo is moved or deleted we clean up the watcher
