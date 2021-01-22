59e0b4249149ae5bc763302092b50856c07f5185
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-01-22 08:35:01 +0200

    e2e: create new project on choosing `Choose different name`.

--- frontend/cypress/integration/uploadProject.spec.js
+++ frontend/cypress/integration/uploadProject.spec.js
@@ -69,7 +69,7 @@ describe('Upload project', () => {
     cy.get('[data-cy=collision-update]').should('be.enabled')
   })
 
-  it('Should commit files to the same project on choosing `Update existing project`', () => {
+  it('Should commit files to the same project on `Update existing project`', () => {
     // Dropping a single file with the same name as an existing project(example)
     // will trigger a name collision
 
@@ -100,6 +100,47 @@ describe('Upload project', () => {
     cy.url().should('eq', `${updateProjectUrl}/${username}/example`)
 
     // The new file is committed and on the update page
+    cy.get('[data-cy=file-name]').contains('example2.png')
+  })
+
+  it('Should create a project and redirect to its update route on `Choose different name`', () => {
+       // Dropping a single file with the same name as an existing project(example)
+    // will trigger a name collision
+
+    // This will match any request made by `utils/giteaApi.createRepo`,
+    // The `**` for matching the csrf query param.
+    cy.intercept(`${giteaApiUrl}/user/repos**`).as('createRepo')
+
+    // This will match any request for `utils/giteaApi.getRepo`
+    cy.intercept(`${giteaApiUrl}/repos/${username}/**`).as('getRepo')
+
+    cy.visit('/projects/new')
+
+    // Simulate dropping a single file('example.png') in the dropzone.
+    cy.fixture('example.png', 'base64').then(f1 => {
+      cy.fixture('example2.png', 'base64').then(f2 => {
+        cy.get('.dropzone').dropFiles([f1, f2], ['example.png', 'example2.png'])
+      })
+    })
+
+    cy.wait(['@createRepo', '@getRepo'])
+
+    // Choose `Choose different name`
+    const newName = 'example2'
+    cy.get('input[name=name]').clear().type(newName)
+
+    // On changing the project name the `Update existing project` button should be disabled
+    cy.get('[data-cy=collision-update]').should('be.disabled')
+
+    cy.get('[data-cy=collision-different-name]').click()
+
+    cy.wait('@getRepo')
+
+    // Clicking the `Choose different name` should create a new repo and redirects to its update page
+    cy.url().should('eq', `${updateProjectUrl}/${username}/${newName}?create=true`)
+
+    // Both files dropped should be in the update page
+    cy.get('[data-cy=file-name]').contains('example.png')
     cy.get('[data-cy=file-name]').contains('example2.png')
   })
 })
