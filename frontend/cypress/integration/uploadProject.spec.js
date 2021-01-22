742d9a0c309f2467586b5db60b3d8cc66cdac5ab
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-01-22 05:13:11 +0200

    e2e: validate projects' names collisions.
    - Uploading a file that will trigger a name collision should open modal and give the user options for either choosing a different name or update the existing project.

--- frontend/cypress/integration/uploadProject.spec.js
+++ frontend/cypress/integration/uploadProject.spec.js
@@ -11,6 +11,10 @@ describe('Upload project', () => {
   before(() => {
     cy.clearCookies()
     cy.createUser(username, email, password)
+  })
+
+  beforeEach(() => {
+    cy.clearCookies()
     cy.visit('/login')
     cy.signIn(username, password)
   })
@@ -33,5 +37,34 @@ describe('Upload project', () => {
     // Wait until getting a response from the server then validate a redirection has happened
     cy.wait(['@createRepo', '@getRepo'])
     cy.url().should('eq', `${updateProjectUrl}/${username}/example?create=true`)
+  })
+
+  it('Should show modal on project names collision', () => {
+    // Dropping a single file with the same name as an existing project(example)
+    // will trigger a name collision
+
+    // This will match any request made by `utils/giteaApi.createRepo`,
+    // The `**` for matching the csrf query param.
+    cy.intercept(`${giteaApiUrl}/user/repos**`).as('createRepo')
+
+    // This will match any request for `utils/giteaApi.getRepo`
+    cy.intercept(`${giteaApiUrl}/repos/${username}/**`).as('getRepo')
+
+    cy.visit('/projects/new')
+
+
+    // Simulate dropping a single file('example.png') in the dropzone.
+    cy.fixture('example.png', 'base64').then(file => {
+      cy.get('.dropzone').dropFile(file, 'example.png')
+    })
+
+    cy.wait(['@createRepo', '@getRepo'])
+
+    // Collision modal should open
+    cy.get('[data-cy=collision-modal]').should('be.visible')
+
+    // `Choose different name` should be disabled as the user haven't changed the name yet
+    cy.get('[data-cy=collision-different-name]').should('be.disabled')
+    cy.get('[data-cy=collision-update]').should('be.enabled')
   })
 })
