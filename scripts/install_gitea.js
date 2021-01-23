0770ab7ea4a1a4c70a2a37f30fb356d592ecafcd
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-01-23 18:49:55 +0200

    Add install_gitea.js script.

---
+++ scripts/install_gitea.js
@@ -0,0 +1,19 @@
+/**
+ * Installs gitea in default configuration and creates admin
+ */
+
+const puppeteer = require('puppeteer')
+
+;(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('http://gitea.kitspace.test:3000/install')
+
+  await page.type('#admin_name', process.env.CYPRESS_GITEA_ADMIN_USERNAME)
+  await page.type('#admin_passwd', process.env.CYPRESS_GITEA_ADMIN_PASSWORD)
+  await page.type('#admin_confirm_passwd', process.env.CYPRESS_GITEA_ADMIN_PASSWORD)
+  await page.type('#admin_email', 'admin@kitspace.com')
+  await page.click('form > div > button')
+
+  await browser.close()
+})()
