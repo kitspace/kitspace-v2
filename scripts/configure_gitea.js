179368b61abb45a17efd8c4a0df52bee90750bd0
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-01-24 03:52:53 +0200

    Add options to puppeteer launcher.

--- scripts/configure_gitea.js
+++ scripts/configure_gitea.js
@@ -5,7 +5,7 @@
 const puppeteer = require('puppeteer')
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage()
   await page.goto('http://gitea.kitspace.test:3000/install')
 
