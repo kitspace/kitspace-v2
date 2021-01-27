8c0bc7bb34d697b29736d223a638be50fd5c4ab8
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-01-27 08:57:53 +0200

    Add install_gitea.sh
    - Still needs replace the hard coded values with environment variables wherever possible

---
+++ scripts/install_gitea.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script depends on `httpie`, install it from here `https://httpie.io/docs#linux`.
+
+set -Eeuo pipefail
+
+docker-compose up &
+
+# Look(retry every 3s) in the logs for sign that gitea is ready
+until docker logs kitspace_gitea_1 | grep 'Preparing to run install page' ; do sleep 3s; done
+
+sleep 5s
+
+# Make a GET request to installation page, as if visiting '/index'
+http --session=/tmp/session.json --headers gitea.kitspace.test:3000
+
+http --session=/tmp/session.json --form --headers --check-status --ignore-stdin gitea.kitspace.test:3000 db_type='PostgreSQL' \
+	db_host='postgres:5432' \
+	db_user='gitea' \
+	db_passwd='change_me' \
+	db_name='gitea' \
+	ssl_mode='disable' \
+	charset='utf8' \
+	db_path='/data/gitea/gitea.db' \
+	app_name='Gitea: Git with a cup of tea' \
+	repo_root_path='/data/git/repositories' \
+	lfs_root_path='/data/git/lfs' \
+	run_user='git' \
+	domain='gitea.kitspace.test' \
+	ssh_port='22' \
+	http_port='3000' \
+	app_url='http://gitea.kitspace.test:3000/' \
+	log_root_path='/data/gitea/log' \
+	enable_federated_avatar='on' \
+	admin_name='__admin' \
+	admin_passwd='123456' \
+	admin_confirm_passwd='123456' \
+	admin_email='admin@kitspace.com'
+
+	sleep 5s && docker-compose kill
