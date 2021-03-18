dcd41b5eaf829f524e3293d3f7e186410e1d8ec6
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-03-18 13:27:41 +0200

    Add `/[username]/[projectName]` routes.

---
+++ frontend/src/pages/[username]/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { isEmpty } from 'lodash'
+
+import { Page } from '@components/Page'
+import ProjectCard from '@components/ProjectCard'
+import { getUserRepos, userExists } from '@utils/giteaApi'
+import styles from './username.module.scss'
+import { useUserRepos } from '@hooks/Gitea'
+
+export const getServerSideProps = async ({ params }) => {
+    console.log(params)
+    const userRepos = await getUserRepos(params.username)
+
+    if (isEmpty(userRepos) && !(await userExists(params.username))) {
+        return {
+            notFound: true,
+        }
+    } else {
+        return {
+            props: {
+                userRepos,
+                username: params.username,
+            },
+        }
+    }
+}
+
+const User = ({ userRepos, username }) => {
+    const { repos: projects } = useUserRepos(username, { initialData: userRepos })
+
+    return (
+        <Page title="home">
+            <h1>Projects by {username}</h1>
+            <div className={styles}>
+                {projects?.map(project => (
+                    <ProjectCard {...project} key={project.id} />
+                ))}
+            </div>
+        </Page>
+    )
+}
+
+export default User
