f8e8b1647cf794bd5fe43cf29311f00fd3f91ef1
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-03-25 22:53:23 +0000

    Avoid looping csrf around on server side

--- orders/src/app.js
+++ orders/src/app.js
@@ -1,22 +1,20 @@
 const express = require('express')
-const fetch = require('isomorphic-unfetch')
+const fetch = require('node-fetch')
 
 function createApp() {
   const app = express()
-  app.use('*', async function (req, res, next) {
-    const csrf = await fetch('http://gitea:3000/api/swagger', {
-      headers: {
-        cookie: req.headers.cookie,
-      },
-    }).then(r => r.headers.get('x-csrf-token'))
+  app.use('*', async (req, res, next) => {
     const user = await fetch('http://gitea:3000/api/v1/user', {
       headers: {
-        cookie: req.headers.cookie,
+        ...req.headers,
         accept: 'application/json',
-        'x-csrf-token': csrf,
       },
     }).then(r => r.json())
-    res.send(user)
+    req.session = { user }
+    next()
+  })
+  app.get('/', (req, res) => {
+    res.send(req.session.user)
   })
   return app
 }
