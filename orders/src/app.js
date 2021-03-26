14aad08a71009172d54bebc0a13b973bbe6bc967
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-03-26 15:52:16 +0000

    Sort out CORS for orders

--- orders/src/app.js
+++ orders/src/app.js
@@ -1,21 +1,46 @@
 const express = require('express')
 const fetch = require('node-fetch')
 
+const { ALLOWED_CORS_DOMAINS } = require('./env')
+
 function createApp() {
   const app = express()
+
+  app.use('*', (req, res, next) => {
+    const origin = req.get('origin')
+    if (!origin) {
+      return next()
+    }
+    if (ALLOWED_CORS_DOMAINS.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin)
+      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS')
+      res.header('Access-Control-Allow-Headers', 'Content-Type')
+      res.header('Access-Control-Allow-Credentials', 'true')
+      return next()
+    }
+    return res.sendStatus(403)
+  })
+
   app.use('*', async (req, res, next) => {
     const user = await fetch('http://gitea:3000/api/v1/user', {
       headers: {
         ...req.headers,
         accept: 'application/json',
       },
-    }).then(r => r.json())
+    }).then(r => {
+      if (r.status >= 200 && r.status < 400) {
+        return r.json()
+      }
+      return r.text()
+    })
+
     req.session = { user }
-    next()
   })
+
   app.get('/', (req, res) => {
     res.send(req.session.user)
   })
+
   return app
 }
 
