d0ed88dc0ef34dfd935894a8c91ad426ce2a1997
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-04-01 15:12:57 +0200

    Delete `useSearchRepo` hook.
    - Relying on the data returned by next from ssr is totally sufficient;
    no need for a hook, next handle updating the data for us.

--- frontend/src/models/SearchFrom.js
+++ frontend/src/models/SearchFrom.js
@@ -1,6 +1,6 @@
 import Joi from 'joi'
 
 export const SearchFromModel = Joi.object({
-    _csrf: Joi.string(),
-    query: Joi.string().required().max(60),
+  _csrf: Joi.string(),
+  query: Joi.string().required().max(60),
 })
