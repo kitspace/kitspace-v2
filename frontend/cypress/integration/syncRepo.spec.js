c8ae10bf677cb815542950d2485a0b00204c756d
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-04-25 10:32:02 +0200

    fixup! Increase timeout for flaky commands.

--- frontend/cypress/integration/syncRepo.spec.js
+++ frontend/cypress/integration/syncRepo.spec.js
@@ -10,7 +10,7 @@ describe('Syncing a project behavior validation', () => {
 
   it('should sync a repo on gitea', () => {
     cy.clearCookies()
-    cy.intercept('http://gitea.kitspace.test:3000/user/kitspace/**')
+    cy.intercept('http://gitea.kitspace.test:3000/user/kitspace/**').as('@sign_in')
 
     cy.createUser(username, email, password)
     cy.visit('/login')
