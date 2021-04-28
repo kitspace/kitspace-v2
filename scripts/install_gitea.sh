0d6b1fc8ff8e7dc3556712845188508b8b00b6aa
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-04-28 16:46:58 +0100

    Use dev docker-compose for e2e tests in CI

--- scripts/install_gitea.sh
+++ scripts/install_gitea.sh
@@ -2,7 +2,7 @@
 
 set -Eeuo pipefail -o verbose
 
-docker-compose -f docker-compose.yml -f docker-compose.deploy.yml up &
+docker-compose up gitea &
 
 until docker logs kitspace_gitea_1 | grep 'ORM engine initialization successful' ; do sleep 3s; done
 
