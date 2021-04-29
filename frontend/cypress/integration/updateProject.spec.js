ab78999883d16f6a23e3e7b27899f6808894298e
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-04-29 23:37:16 +0200

    e2e: make sure project page has loaded.

--- frontend/cypress/integration/updateProject.spec.js
+++ frontend/cypress/integration/updateProject.spec.js
@@ -164,9 +164,6 @@ describe('Update project form validation', () => {
 
     // Create a repo by uploading files
     cy.visit('/projects/new') // go back to `projects/new` page as the previous step `sync` will trigger redirect
-    cy.intercept(
-      `http://gitea.kitspace.test:3000/api/v1/users/${username}/repos`,
-    ).as('getRepos')
 
     // Simulate dropping a single file('example.png') in the dropzone.
     cy.fixture('example.png', 'base64').then(file => {
