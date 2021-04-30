f0e5e8c1a1fe07bab270bd27a471ebca8fd32c47
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-04-30 03:05:58 +0200

    CI: fix `auto_gen_fixtures_path`.

--- scripts/pre_e2e.sh
+++ scripts/pre_e2e.sh
@@ -3,6 +3,6 @@ set -Eeuo pipefail
 
 # Generate big files which is during testing
 # `uploadProject.spec.js` -> should reject files bigger than `MAX_FILE_SIZE`
-auto_gen_fixtures_path=frontend/cypress/fixtures/auto-gen
+auto_gen_fixtures_path=e2e/cypress/fixtures/auto-gen
 mkdir $auto_gen_fixtures_path
 truncate -s "${MAX_FILE_SIZE}" "${auto_gen_fixtures_path}/big.txt"
