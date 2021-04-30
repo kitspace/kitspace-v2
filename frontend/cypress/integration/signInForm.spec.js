db3768e6aa9262dc585fc0cd675e234521aa5abd
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-04-30 13:08:29 +0200

    Revert "e2e: upgrade cypress@7.1.0".."Use different version of gitea."
    This reverts commit 677886df50712a7e6eec446de28e3f9ce699bfd2.

--- frontend/cypress/integration/signInForm.spec.js
+++ frontend/cypress/integration/signInForm.spec.js
@@ -1,16 +1,6 @@
 import faker from 'faker'
-import Joi from 'joi'
 
-const SignInFormModel = Joi.object({
-  _csrf: Joi.string(),
-  username: Joi.string()
-    .regex(/^(?:[A-Z\d][A-Z\d_-]{0,19}|[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})$/i)
-    .messages({
-      'string.pattern.base': 'Invalid username or email',
-    }),
-  password: Joi.string().required(),
-  remember: Joi.boolean(),
-})
+import { SignInFormModel } from '../../src/models/SignInForm'
 
 describe('Log in form validation', () => {
   before(() => {
--- frontend/cypress/integration/syncRepo.spec.js
+++ frontend/cypress/integration/syncRepo.spec.js
@@ -13,6 +13,7 @@ describe('Syncing a project behavior validation', () => {
     cy.visit('/')
   })
 
+  
   it('should sync a repo on gitea', () => {
     cy.clearCookies()
     cy.intercept('http://gitea.kitspace.test:3000/user/kitspace/**').as('sign_in')
@@ -21,7 +22,7 @@ describe('Syncing a project behavior validation', () => {
     cy.visit('/login')
     cy.signIn(username, password)
     cy.wait('@sign_in')
-
+    
     cy.visit('/projects/new')
 
     cy.intercept('http://gitea.kitspace.test:3000/api/v1/repos/migrate**').as(
@@ -29,7 +30,7 @@ describe('Syncing a project behavior validation', () => {
     )
 
     cy.url().then(url => {
-      if (!url.endsWith('/projects/new')) {
+      if(!url.endsWith('/projects/new')) {
         cy.visit('/projects/new')
       }
     })
