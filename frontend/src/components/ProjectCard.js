99aad2b0477be6ba1e7c4604b2687ac5ad6193bb
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-05-24 16:52:41 +0100

    Make project card a link

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { string, object } from 'prop-types'
-
 import { Card } from 'semantic-ui-react'
+import Link from 'next/link'
+
 import useThumbnail from '@hooks/useThumbnail'
 import styles from './ProjectCard.module.scss'
 
 const ProjectCard = ({ name, full_name, description, owner }) => {
   const { src, isLoading, isError } = useThumbnail(full_name)
   return (
-    <Card>
-      <div className={styles.thumbnail}>
-        <div>{isLoading || isError ? null : <img src={src} />}</div>
-      </div>
-      <Card.Content>
-        <Card.Header>{name}</Card.Header>
-        <Card.Meta>{owner.username}</Card.Meta>
-        <Card.Description>{description}</Card.Description>
-      </Card.Content>
-    </Card>
+    <Link href={full_name}>
+      <Card className={styles.card}>
+        <div className={styles.thumbnail}>
+          <div>{isLoading || isError ? null : <img src={src} />}</div>
+        </div>
+        <Card.Content>
+          <Card.Header>{name}</Card.Header>
+          <Card.Meta>{owner.username}</Card.Meta>
+          <Card.Description>{description}</Card.Description>
+        </Card.Content>
+      </Card>
+    </Link>
   )
 }
 
