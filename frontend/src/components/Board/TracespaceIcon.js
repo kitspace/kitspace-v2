d1ca94d7508e9756285265168975add97427ed61
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-05-27 12:36:21 +0200

    UI: fix `TracespaceIcon` color.
    - In the original kitspace the color was in `board_showcas.scss`, see
    https://github.com/kitspace/kitspace/blob/a67656c345832adbfa01419d6b09af3aef66b655/src/page/board_showcase.scss#L131.
    - In v2 all components are self contained.

--- frontend/src/components/Board/TracespaceIcon.js
+++ frontend/src/components/Board/TracespaceIcon.js
@@ -6,7 +6,7 @@ const TracespaceIcon = () => {
       id="traceSpaceLogo"
       viewBox="0 0 36 36"
       height="18"
-      style={{ marginRight: 10 }}
+      style={{ marginRight: 10, fill: '#30c2c2' }}
     >
       <path d="M22 14H36V0H16L0 16V36H14V22L22 14Z M36 28C36 32.4183 32.4183 36 28 36C23.5817 36 20 32.4183 20 28C20 23.5817 23.5817 20 28 20C32.4183 20 36 23.5817 36 28Z" />
     </svg>
