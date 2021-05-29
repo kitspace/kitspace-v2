81108829ee74a090da053a79551a3a93402f4f5a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-05-29 16:14:02 +0100

    Fix import order in cypress/support/index.js

--- frontend/cypress/support/index.js
+++ frontend/cypress/support/index.js
@@ -1,6 +1,6 @@
 import 'cypress-fail-fast'
 import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector.js'
 
-installLogsCollector()
-
 import './commands'
+
+installLogsCollector()
