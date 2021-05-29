kitspace/kitspace-using-gitea

Refactor: rename `reqSign...` -> `requireSign...`.
- Before `req` was used as the same convention used by gitea.

frontend/src/pages/login.js
@@ -32,7 +32,7 @@ const Login = () => {
   }, [query])
 
   return (
-    <Page title="login" reqSignOut>
+    <Page title="login" requireSignO>
       <Grid style={{ maxWidth: '500px', margin: 'auto' }} verticalAlign="middle">
         <Grid.Column>
           <Tab

kitspace/kitspace-v2

frontend/src/pages/login.js
@@ -32,7 +32,7 @@ const Login = () => {
   }, [query])
 
   return (
-    <Page title="login" reqSignOut>
+    <Page title="login" requireSignOut>
       <Grid style={{ maxWidth: '500px', margin: 'auto' }} verticalAlign="middle">
         <Grid.Column>
           <Tab