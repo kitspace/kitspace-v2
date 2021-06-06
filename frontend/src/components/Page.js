dc548318487219e5a882e393496d3b1342f2c98c
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-06-07 00:38:30 +0200

    Fix: `Page` component.
    - Update its content to match project-page branch.

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { string, bool, node } from 'prop-types'
 
-import { Container, Loader } from 'semantic-ui-react'
+import { Loader } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
 import Head from './Head'
 import NavBar from './NavBar'
+import styles from './Page.module.scss'
 
-const Content = ({ requireSignIn, requireSignOut, children }) => {
+const Content = ({ requireSignIn, requireSignOut, contentFullSize, children }) => {
   const { push, pathname } = useRouter()
   const [loading, setLoading] = useState(true)
 
@@ -29,14 +30,31 @@ const Content = ({ requireSignIn, requireSignOut, children }) => {
       </Loader>
     )
   }
-  return <Container style={{ marginTop: 30 }}>{children}</Container>
+  return (
+    <div
+      className={contentFullSize ? styles.minimalContainer : styles.container}
+      data-cy="page-container"
+    >
+      {children}
+    </div>
+  )
 }
 
-const Page = ({ title, requireSignIn, requireSignOut, children }) => (
+const Page = ({
+  title,
+  requireSignIn,
+  requireSignOut,
+  contentFullSize,
+  children,
+}) => (
   <>
     <Head title={title} />
     <NavBar />
-    <Content requireSignIn={requireSignIn} requireSignOut={requireSignOut}>
+    <Content
+      requireSignIn={requireSignIn}
+      requireSignOut={requireSignOut}
+      contentFullSize={contentFullSize}
+    >
       {children}
     </Content>
   </>
@@ -46,17 +64,20 @@ Page.propTypes = {
   title: string.isRequired,
   requireSignIn: bool,
   requireSignOut: bool,
+  contentFullSize: bool,
   children: node.isRequired,
 }
 
 Page.defaultProps = {
   requireSignIn: false,
   requireSignOut: false,
+  contentFullSize: false,
 }
 
 Content.propTypes = {
   requireSignIn: bool.isRequired,
   requireSignOut: bool.isRequired,
+  contentFullSize: bool.isRequired,
   children: node.isRequired,
 }
 
