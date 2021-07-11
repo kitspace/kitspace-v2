b324733278d3e09114c1c1e37123941bb4467c62
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-07-11 15:38:52 +0200

    e2e: Add `should display card thumbnails`.

--- frontend/cypress/integration/multiPart.spec.js
+++ frontend/cypress/integration/multiPart.spec.js
@@ -1,22 +1,30 @@
 import faker from 'faker'
 
 describe('Render correct numbers of cards', () => {
-  const username = faker.name.firstName()
-  const email = faker.internet.email()
-  const password = '123456'
-
-  const syncedRepoUrl = 'https://github.com/kitspace-forks/DIY_particle_detector'
+  const syncedRepoUrlMultiParts =
+    'https://github.com/kitspace-forks/DIY_particle_detector'
+  const syncedRepoUrl = 'https://github.com/kitspace-forks/CH330_Hardware'
   // Currently hardcoded but preferably it should get it from the yaml
   const multiPartsNames = ['alpha-spectrometer', 'electron-detector']
-  const repoName = syncedRepoUrl.split('/').slice(-1).toString()
+  const multiPartsRepoName = syncedRepoUrlMultiParts.split('/').slice(-1).toString()
+  const normalRepoName = 'CH330_Hardware'
 
   before(() => {
     // visit home before running the tests, instead of using `wait`.
     cy.visit('/')
   })
 
-  it('should render a card for each sub project', () => {
+  beforeEach(() => {
     cy.clearCookies()
+  })
+
+  it('should render a card for each sub project', () => {
+    const username = faker.name.firstName()
+    const email = faker.internet.email()
+    const password = '123456'
+
+    const repoName = syncedRepoUrlMultiParts.split('/').slice(-1).toString()
+
     cy.intercept('http://gitea.kitspace.test:3000/user/kitspace/**').as('sign_in')
 
     cy.createUser(username, email, password)
@@ -34,8 +42,8 @@ describe('Render correct numbers of cards', () => {
       }
     })
 
-    // Migrate the repo
-    cy.get('input:first').type(syncedRepoUrl)
+    // Migrate the multipart repo
+    cy.get('input:first').type(syncedRepoUrlMultiParts)
     cy.get('button').contains('Sync').click()
 
     // Wait for redirection for project page
@@ -48,5 +56,63 @@ describe('Render correct numbers of cards', () => {
     multiPartsNames.forEach(name => {
       cy.get('.ui.card').contains(name)
     })
+  })
+
+  it('should display card thumbnails', () => {
+    const username = faker.name.firstName()
+    const email = faker.internet.email()
+    const password = '123456'
+
+    cy.intercept('http://gitea.kitspace.test:3000/user/kitspace/**').as('sign_in')
+
+    cy.createUser(username, email, password)
+    cy.visit('/login')
+    cy.signIn(username, password)
+    cy.wait('@sign_in')
+
+    cy.visit('/projects/new')
+
+    cy.intercept('http://gitea.kitspace.test:3000/api/v1/repos/migrate**')
+
+    cy.url().then(url => {
+      if (!url.endsWith('/projects/new')) {
+        cy.visit('/projects/new')
+      }
+    })
+
+    // Migrate the multipart repo
+    cy.get('input:first').type(syncedRepoUrlMultiParts)
+    cy.get('button').contains('Sync').click()
+
+    // Wait for redirection for project page
+    cy.url().should('contain', `${username}/${multiPartsRepoName}`)
+    // Wait for the repo to finish migration, by checking if sync message has appeared.
+    cy.get('[data-cy=sync-msg]', { timeout: 60_000 }).should('be.visible')
+
+    /* Migrate the normal repo */
+    cy.visit('/projects/new')
+
+    cy.intercept('http://gitea.kitspace.test:3000/api/v1/repos/migrate**')
+
+    cy.url().then(url => {
+      if (!url.endsWith('/projects/new')) {
+        cy.visit('/projects/new')
+      }
+    })
+
+    cy.get('input:first').type(syncedRepoUrl)
+    cy.get('button').contains('Sync').click()
+
+    // Wait for redirection for project page
+    cy.url().should('contain', `${username}/${normalRepoName}`)
+    // Wait for the repo to finish migration, by checking if sync message has appeared.
+    cy.get('[data-cy=sync-msg]', { timeout: 60_000 }).should('be.visible')
+
+    cy.visit(`/${username}`)
+    // There should be 3 thumbnails = 2 form multiparts + 1 normal project
+    cy.get('[data-cy=project-card-thumbnail]').should(
+      'have.length',
+      multiPartsNames.length + 1,
+    )
   })
 })
