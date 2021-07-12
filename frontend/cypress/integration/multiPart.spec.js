44e29bb555fd86264fb10ce9ee07e054af09ae5a
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-07-12 11:52:30 +0200

    e2e: Add `should redirect to multi project page`.

--- frontend/cypress/integration/multiPart.spec.js
+++ frontend/cypress/integration/multiPart.spec.js
@@ -50,7 +50,7 @@ describe('Render project cards', () => {
     cy.visit(`/${username}`)
 
     multiPartsNames.forEach(name => {
-      cy.get('.ui.card').contains(name)
+      cy.get('[data-cy=project-card]').contains(name)
     })
   })
 
@@ -109,6 +109,48 @@ describe('Render project cards', () => {
     cy.get('[data-cy=project-card-thumbnail]').should(
       'have.length',
       multiPartsNames.length + 1,
+    )
+  })
+
+  it('should redirect to the multi project page', () => {
+    const username = faker.name.firstName()
+    const email = faker.internet.email()
+    const password = '123456'
+
+    cy.intercept('http://gitea.kitspace.test:3000/user/kitspace/**').as('sign_in')
+
+    cy.createUser(username, email, password)
+    cy.visit('/login')
+    cy.signIn(username, password)
+    cy.wait('@sign_in')
+
+    cy.visit('/projects/new')
+
+    cy.intercept('http://gitea.kitspace.test:3000/api/v1/repos/migrate**')
+
+    cy.url().then(url => {
+      if (!url.endsWith('/projects/new')) {
+        cy.visit('/projects/new')
+      }
+    })
+
+    // Migrate the multipart repo
+    cy.get('input:first').type(syncedRepoUrlMultiParts)
+    cy.get('button').contains('Sync').click()
+
+    // Wait for redirection for project page
+    cy.url().should('contain', `${username}/${multiPartsRepoName}`)
+    // Wait for the repo to finish migration, by checking if sync message has appeared.
+    cy.get('[data-cy=sync-msg]', { timeout: 60_000 }).should('be.visible')
+
+    // Go to the home page and click on a multipart project card
+    cy.visit('/')
+    cy.get('[data-cy=project-card]').contains(multiPartsNames[0]).click()
+
+    // Should redirect to the `[username]/[projectName]/[multiProject]`
+    cy.url().should(
+      'contain',
+      `${username}/${multiPartsRepoName}/${multiPartsNames[0]}`,
     )
   })
 })
