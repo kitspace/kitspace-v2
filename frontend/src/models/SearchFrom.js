6be51c53e4ef9d9fd880bd10148e43ccece3e74c
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-07-28 10:48:54 +0200

    Fix: only redirect to /search after submitting.
    - Create a `Search` button.
    - Redirect to `/search` after clicking on the `Search` button.

--- frontend/src/models/SearchFrom.js
+++ frontend/src/models/SearchFrom.js
@@ -1,8 +1,8 @@
 import Joi from 'joi'
 
-const SearchFromModel = Joi.object({
+const SearchFormModel = Joi.object({
   _csrf: Joi.string(),
   query: Joi.string().required().max(60),
 })
 
-export default SearchFromModel
+export default SearchFormModel
