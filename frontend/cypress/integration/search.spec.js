4a73ad3b6dafaa5dd7a95953adf128406878fff5
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-07-29 09:58:21 +0200

    core: fetch search results with swr in `/search`.
    This was a bit tricky:
    
    - The form and the projects grid can't be in the same component;changes to
      form triggers rerendering which triggers `useSearchRepos`, and the
      form was created in the first place to prevent DDOSing the search
      endpoint.
    - On submitting search form, the path gets updated swr is mutated.

--- frontend/cypress/integration/search.spec.js
+++ frontend/cypress/integration/search.spec.js
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Homepage search bar', () => {
   it('should redirect to /search on clicking `Search` button', () => {
     const queryTerm = 'awesome project'
@@ -12,5 +14,49 @@ describe('Homepage search bar', () => {
     cy.get('[data-cy=search-button]').click({ force: true })
     // Should redirect to the search page
     cy.url().should('include', `/search?q=${encodeURI(queryTerm)}`)
+  })
+
+  it('should display project card on submitting search form', () => {
+    const username = faker.unique(faker.name.firstName)
+    const email = faker.unique(faker.internet.email)
+    const password = '123456'
+
+    const repoName = 'CH330_Hardware'
+    const syncedRepoUrl = 'https://github.com/kitspace-forks/CH330_Hardware'
+
+    cy.intercept('http://gitea.kitspace.test:3000/user/kitspace/**').as('sign_in')
+
+    cy.createUser(username, email, password)
+    cy.visit('/login')
+    cy.signIn(username, password)
+    cy.wait('@sign_in')
+
+    cy.visit('/projects/new')
+
+    cy.url().then(url => {
+      if (!url.endsWith('/projects/new')) {
+        cy.visit('/projects/new')
+      }
+    })
+
+    // Migrate the repo
+    cy.get('input:first').type(syncedRepoUrl)
+    cy.get('button').contains('Sync').click()
+
+    // Wait for redirection for project page
+    cy.url({ timeout: 60_000 }).should('contain', `${username}/${repoName}`)
+    // Wait for the repo to finish migration, by checking the visibility of processing-loader.
+    cy.get('[data-cy=processing-loader]', { timeout: 60_000 })
+    // Wait for the repo to finish processing, by checking the visibility of info-bar.
+    cy.get('[data-cy=info-bar]', { timeout: 60_000 }).should('be.visible')
+
+    cy.visit('/')
+
+    cy.get('[data-cy=search-field] > input').type(repoName)
+    // Click on the `Search` button
+    cy.get('[data-cy=search-button]').click({ force: true })
+    // Should redirect to the search page
+    cy.url().should('include', `/search?q=${encodeURI(repoName)}`)
+    cy.get('[data-cy=project-card]').should('have.length.gte', 1)
   })
 })
