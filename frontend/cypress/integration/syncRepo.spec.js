31c23b4ccf33d8f0913a0e36f785dc77b3bbe09a
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-08-03 11:38:16 +0200

    e2e: stop intercepting gtiea `sign_in` endpoint.
    - Instead of waiting for the endpoint to be called, look for the `Log Out` button.

--- frontend/cypress/integration/syncRepo.spec.js
+++ frontend/cypress/integration/syncRepo.spec.js
@@ -24,8 +24,6 @@ describe('Syncing a project behavior validation', () => {
     cy.signIn(username, password)
     cy.get('[data-cy=logout-button]')
 
-    cy.forceVisit('/projects/new')
-
     cy.get('input:first').type(syncedRepoUrl)
     cy.get('button').contains('Sync').click()
     cy.get('[data-cy=sync-result-message]').should('have.class', 'green')
