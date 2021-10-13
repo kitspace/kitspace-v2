b0fa50c8cdad12ca6cb83adb1f0eaf5b253abc34
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-10-13 14:08:27 +0200

    Add `/settings` page

---
+++ frontend/src/pages/settings.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Page from '@components/Page'
+
+const Settings = () => {
+  return (
+    <Page requireSignIn>
+      <h1>Settings</h1>
+    </Page>
+  )
+}
+
+export default Settings
