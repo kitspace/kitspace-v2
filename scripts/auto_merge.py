2a5b9f765337d7da5a1a528da0df4249ac58498b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2022-01-29 18:28:44 +0000

    Split out github_api utility

--- scripts/auto_merge.py
+++ scripts/auto_merge.py
@@ -1,71 +1,6 @@
 #!/usr/bin/env python3
-from __future__ import print_function
-import json
-import os
-import sys
 import subprocess
-import urllib.request
-import urllib.parse
-
-GITHUB_TOKEN = sys.argv[1]
-GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
-GITHUB_RUN_ID = os.environ["GITHUB_RUN_ID"]
-GITHUB_RUN_URL = f"https://github.com/{GITHUB_REPOSITORY}/runs/{GITHUB_RUN_ID}"
-HEADERS = {
-    "Accept": "application/vnd.github.v3+json",
-    "Authorization": f"token {GITHUB_TOKEN}",
-    "Content-Type": "application/json",
-}
-
-
-def get_pulls():
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/pulls"
-    request = urllib.request.Request(url, method="GET", headers=HEADERS)
-    response = urllib.request.urlopen(request).read()
-    return json.loads(response)
-
-
-def get_comments(issue_number, page=1):
-    page_size = 30
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues/{issue_number}/comments?page={page}&per_page={page_size}"
-    request = urllib.request.Request(url, method="GET", headers=HEADERS)
-    response = urllib.request.urlopen(request).read()
-    comments = json.loads(response)
-    if len(comments) == page_size:
-        comments += get_comments(issue_number, page=page + 1)
-    return comments
-
-
-def delete_bot_comments(issue_number):
-    comments = get_comments(issue_number)
-    for comment in comments:
-        if comment["user"]["login"] == "github-actions[bot]":
-            url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues/comments/{comment['id']}"
-            request = urllib.request.Request(url, method="DELETE", headers=HEADERS)
-            urllib.request.urlopen(request)
-
-
-def post_comment(issue_number, message):
-    delete_bot_comments(issue_number)
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues/{issue_number}/comments"
-    data = json.dumps({"body": message}).encode("utf-8")
-    request = urllib.request.Request(url, method="POST", headers=HEADERS, data=data)
-    urllib.request.urlopen(request)
-
-
-def create_commit_status(sha, state, description):
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/statuses/{sha}"
-    data = json.dumps(
-        {
-            "state": state,
-            "description": description,
-            "target_url": GITHUB_RUN_URL,
-            "context": "auto-merge: review",
-        }
-    ).encode("utf-8")
-    request = urllib.request.Request(url, method="POST", headers=HEADERS, data=data)
-    urllib.request.urlopen(request)
-
+import github_api
 
 subprocess.run(
     ["git", "config", "pull.rebase", "false"],
@@ -80,7 +15,7 @@ subprocess.run(
     check=True,
 )
 
-pulls = get_pulls()
+pulls = github_api.get_pulls()
 
 for pull in pulls:
     if not pull["draft"] and pull["head"] is not None:
@@ -91,11 +26,13 @@ for pull in pulls:
                 ["git", "pull", pull["head"]["repo"]["clone_url"], ref], check=True
             )
         except:
-            create_commit_status(
+            github_api.create_commit_status(
                 sha,
                 "failure",
                 'Could not be merged into the "review" branch',
             )
         else:
             print(f'Merged "{pull["head"]["label"]}"')
-            create_commit_status(sha, "pending", 'Successfully merged into the "review" branch')
+            github_api.create_commit_status(
+                sha, "pending", 'Successfully merged into the "review" branch'
+            )
---
+++ scripts/github_api.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import json
+import urllib.request
+import urllib.parse
+
+GITHUB_TOKEN = sys.argv[1]
+GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
+GITHUB_RUN_ID = os.environ["GITHUB_RUN_ID"]
+GITHUB_RUN_URL = f"https://github.com/{GITHUB_REPOSITORY}/runs/{GITHUB_RUN_ID}"
+
+HEADERS = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Content-Type": "application/json",
+}
+
+
+def get_pulls():
+    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/pulls"
+    request = urllib.request.Request(url, method="GET", headers=HEADERS)
+    response = urllib.request.urlopen(request).read()
+    return json.loads(response)
+
+
+def get_comments(issue_number, page=1):
+    page_size = 30
+    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues/{issue_number}/comments?page={page}&per_page={page_size}"
+    request = urllib.request.Request(url, method="GET", headers=HEADERS)
+    response = urllib.request.urlopen(request).read()
+    comments = json.loads(response)
+    if len(comments) == page_size:
+        comments += get_comments(issue_number, page=page + 1)
+    return comments
+
+
+def delete_bot_comments(issue_number):
+    comments = get_comments(issue_number)
+    for comment in comments:
+        if comment["user"]["login"] == "github-actions[bot]":
+            url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues/comments/{comment['id']}"
+            request = urllib.request.Request(url, method="DELETE", headers=HEADERS)
+            urllib.request.urlopen(request)
+
+
+def post_comment(issue_number, message):
+    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/issues/{issue_number}/comments"
+    data = json.dumps({"body": message}).encode("utf-8")
+    request = urllib.request.Request(url, method="POST", headers=HEADERS, data=data)
+    urllib.request.urlopen(request)
+
+
+def create_commit_status(sha, state, description):
+    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/statuses/{sha}"
+    data = json.dumps(
+        {
+            "state": state,
+            "description": description,
+            "target_url": GITHUB_RUN_URL,
+            "context": "auto-merge: review",
+        }
+    ).encode("utf-8")
+    request = urllib.request.Request(url, method="POST", headers=HEADERS, data=data)
+    urllib.request.urlopen(request)
