dd63a5c33ca4bd01ea505c18f7ff6e8f589ff76b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2022-01-29 20:02:04 +0000

    Try and report auto-merge review deploy

---
+++ scripts/report_auto_merge_deploy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import subprocess
+import github_api
+
+merge_parents = subprocess.check_output(
+    ["git", "log", "origin/master..HEAD", "--merges", "--format=%P"],
+).decode("utf-8")
+
+state = sys.argv[1]
+
+for parents in merge_parents.splitlines():
+    parents = parents.split(" ")
+    # always take the second parent of the merge as the sha we are interested in
+    sha = parents[1]
+    if state == "failure":
+        github_api.create_commit_status(
+            sha,
+            "failure",
+            "Merged but could not be deployed",
+        )
+    elif state == "success":
+        github_api.create_commit_status(
+            sha, "success", "Deployed", target_url="https://review.staging.kitspace.dev"
+        )
