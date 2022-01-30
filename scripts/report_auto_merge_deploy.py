b60951914aafa658b4648cbbd22777a7cfa4bae5
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2022-01-30 12:30:07 +0000

    Check email and add additional "pending" status

--- scripts/report_auto_merge_deploy.py
+++ scripts/report_auto_merge_deploy.py
@@ -3,23 +3,40 @@ import sys
 import subprocess
 import github_api
 
-merge_parents = subprocess.check_output(
-    ["git", "log", "origin/master..HEAD", "--merges", "--format=%P"],
-).decode("utf-8")
+# log all merges since master in a format of "%ae %P"
+# which is "<merger-email> <parent1-sha> <parent2-sha>"
+merges = (
+    subprocess.check_output(
+        ["git", "log", "origin/master..HEAD", "--merges", "--format=%ae %P"],
+    )
+    .decode("utf-8")
+    .splitlines()
+)
 
 state = sys.argv[1]
 
-for parents in merge_parents.splitlines():
-    parents = parents.split(" ")
-    # always take the second parent of the merge as the sha we are interested in
-    sha = parents[1]
-    if state == "failure":
-        github_api.create_commit_status(
-            sha,
-            "failure",
-            "Merged but could not be deployed",
-        )
-    elif state == "success":
-        github_api.create_commit_status(
-            sha, "success", "Deployed", target_url="https://review.staging.kitspace.dev"
-        )
+for line in merges:
+    line = line.split(" ")
+    email = line[0]
+    if email == "auto-merge-bot@kitspace.dev":
+        # always take the second parent of the merge as the sha we are interested in
+        sha = line[2]
+        if state == "failure":
+            github_api.create_commit_status(
+                sha,
+                "failure",
+                "Merged but could not be deployed",
+            )
+        elif state == "success":
+            github_api.create_commit_status(
+                sha,
+                "success",
+                "Deployed",
+                target_url="https://review.staging.kitspace.dev",
+            )
+        elif state == "pending":
+            github_api.create_commit_status(
+                sha,
+                "pending",
+                "Building images",
+            )
