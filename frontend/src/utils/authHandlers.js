57f1ede4bfc5e703ad392f6eb961c10141518c11
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2022-02-02 22:36:48 +0000

    Fix redirects not being passed to browser

--- frontend/src/utils/authHandlers.js
+++ frontend/src/utils/authHandlers.js
@@ -4,7 +4,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 /**
  * @param {string} asPath the page path e.g., `/project/new`.
- * @returns
  */
 export const withRequireSignIn =
   asPath =>
@@ -14,9 +13,10 @@ export const withRequireSignIn =
   ({ req, res }) => {
     const session = req?.session ?? window?.session
 
-    if (!isAuthenticated(session)) {
+    const isRelativePath = asPath.startsWith('/')
+    if (!isAuthenticated(session) && isRelativePath) {
       if (res) {
-        res.writeHead(307, { Location: '/login' })
+        res.writeHead(307, { Location: `/login?redirect=${asPath}` })
         res.end()
       } else {
         Router.replace(`/login?redirect=${asPath}`)
