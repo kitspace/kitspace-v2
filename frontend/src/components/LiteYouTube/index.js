9fe152789a6dd93776e9e347d372de802311677b
Ghanem <37152329+AbdulrhmnGhanem@users.noreply.github.com>
2022-02-08 13:12:15 +0200

    Switch to ` @justinribeiro/lite-youtube` for videos (#297)

---
+++ frontend/src/components/LiteYouTube/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { string } from 'prop-types'
+
+import styles from './index.module.scss'
+
+const LiteYouTube = props => {
+  // lite-youtube exports a web component so it has to be loaded on the client-side.
+  useEffect(() => import('@justinribeiro/lite-youtube'), [])
+  return (
+    <div className={props.className}>
+      <lite-youtube {...props}></lite-youtube>
+    </div>
+  )
+}
+
+LiteYouTube.propTypes = {
+  className: string,
+}
+
+LiteYouTube.defaultProps = {
+  className: styles.LiteYouTubeDefault,
+}
+
+export default LiteYouTube
