1b3093092a7dfb997503dec14f863208db1b2e0f
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2022-02-12 12:56:46 +0200

    CI: stop creating admin in `install_gitea` step

--- scripts/install_gitea.sh
+++ scripts/install_gitea.sh
@@ -6,10 +6,4 @@ docker-compose -f docker-compose.yml -f docker-compose.deploy.yml up &
 
 until docker logs kitspace_gitea_1 | grep 'ORM engine initialization successful' ; do sleep 3s; done
 
-docker-compose -f docker-compose.yml -f docker-compose.deploy.yml run gitea \
-	gitea admin user create --admin \
-		--username "${CYPRESS_GITEA_ADMIN_USERNAME}" \
-		--password "${CYPRESS_GITEA_ADMIN_PASSWORD}" \
-		--email admin@example.com
-
 docker-compose stop
