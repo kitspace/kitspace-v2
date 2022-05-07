7545231c695b8e50564c47ed310fd296b4377f09
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2022-05-07 15:10:17 +0100

    Fix grammar in comment in scripts/pre_e2e.sh

--- scripts/pre_e2e.sh
+++ scripts/pre_e2e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-# Generate big files which is used during testing
+# Generate big files which are used during testing
 # `uploadProject.spec.js` -> should reject files bigger than `MAX_FILE_SIZE`
 auto_gen_fixtures_path=e2e/cypress/fixtures/auto-gen
 mkdir $auto_gen_fixtures_path
