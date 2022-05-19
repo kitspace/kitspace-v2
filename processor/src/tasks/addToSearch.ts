10741f2984369c7269e8fbafc4489311dffed19d
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-19 20:11:54 +0100

    Handle multi renames when adding to search

--- processor/src/tasks/addToSearch.ts
+++ processor/src/tasks/addToSearch.ts
@@ -6,6 +6,10 @@ const meili = new MeiliSearch({
   host: 'http://meilisearch:7700',
   apiKey: process.env.MEILI_MASTER_KEY,
 })
+const index = meili.index('projects')
+index.updateSettings({
+  filterableAttributes: ['id', 'multiParentId', 'gitHash'],
+})
 
 export default async function addToSearch(
   job,
@@ -15,12 +19,13 @@ export default async function addToSearch(
     log.warn(`Not adding '${name}' to meilisearch due to missing searchId`)
     return
   }
-  const index = meili.index('projects')
   let multiParentId = null
   if (searchId.split('-').length > 1) {
     multiParentId = searchId.split('-')[0]
   }
+
   const readme = getReadmeAsText(readmeHTML)
+
   const document = {
     id: searchId,
     name,
@@ -33,7 +38,34 @@ export default async function addToSearch(
     multiParentId,
   }
   log.debug(`meilisearch: adding/updating document id='${searchId}'`)
+
   await index.addDocuments([document])
+
+  // multi names can change in kitspace.yaml, the project can also go from a
+  // multi to a single project or vice versa. we need to clear out any
+  // documents that could be left over due to id changes.
+  if (multiParentId == null) {
+    // if we are not a multi project clear out any multi document that refer to
+    // this id as parent (if we went from multi to single)
+    const previousMultis = await index.search('', {
+      filter: `multiParentId = ${searchId}`,
+    })
+    const docIds = previousMultis.map(x => x.id)
+    await index.deleteDocuments(docIds)
+  } else {
+    // if we are a multi project, clear any previous parent (if we went from
+    // single to multi). also clear any documents with the same multiParentId
+    // that are not from this latest git commit hash (if a multi project was
+    // renamed)
+    const [parent, renamedMultis] = await Promise.all([
+      index.search('', { filter: `id = ${multiParentId}` }),
+      index.search('', {
+        filter: `(multiParentId = ${multiParentId}) AND (gitHash != ${hash})`,
+      }),
+    ])
+    const docIds = parent.hits.concat(renamedMultis.hits).map(x => x.id)
+    await index.deleteDocuments(docIds)
+  }
 }
 
 function getReadmeAsText(readmeHTML) {
