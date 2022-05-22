dd243fa87ef351094108df8f8f1bfa92e6776f01
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-22 13:02:00 +0100

    Fix images for readmes that are in folders

--- processor/src/tasks/processReadme.ts
+++ processor/src/tasks/processReadme.ts
@@ -40,7 +40,14 @@ async function processReadme(
   const rawMarkdown = await readFile(readmeInputPath, { encoding: 'utf8' })
   const readmeAsHTML = await renderMarkdown(rawMarkdown)
 
-  const processedReadmeHTML = postProcessMarkdown(readmeAsHTML, ownerName, repoName)
+  const readmeFolder = path.relative(inputDir, readmeInputPath)
+
+  const processedReadmeHTML = postProcessMarkdown(
+    readmeAsHTML,
+    ownerName,
+    repoName,
+    readmeFolder,
+  )
 
   await writeFile(readmePath, processedReadmeHTML)
     .then(() => job.updateProgress({ status: 'done', file: readmePath }))
@@ -79,14 +86,19 @@ async function renderMarkdown(rawMarkdown) {
 /**
  * Make urls absolute not relative
  */
-function postProcessMarkdown(readmeAsHtml, ownerName, repoName) {
+function postProcessMarkdown(readmeAsHtml, ownerName, repoName, readmeFolder) {
   const $ = cheerio.load(readmeAsHtml)
   $('img').each((_, elem) => {
     const img = $(elem)
-    const src = img.attr('src')
+    let src = img.attr('src')
 
     const isRelativeUri = !src.match(/^https?:\/\//)
     if (isRelativeUri) {
+      if (src.startsWith('/')) {
+        src = src.slice(1)
+      } else {
+        src = path.join(readmeFolder, src)
+      }
       const rawUrl = `${GITEA_URL}/${ownerName}/${repoName}/raw/${src}`
       img.attr('src', rawUrl)
       img.attr('data-cy', 'relative-readme-img')
