e1656d0ab04b84d358f95271365e8bbf28f21927
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-22 13:02:00 +0100

    processor: fix relative images for readmes
    (cherry picked from commit 0a799531d9ef2719fa621c1b910598943b45f7d0)

--- processor/src/tasks/processReadme.ts
+++ processor/src/tasks/processReadme.ts
@@ -1,11 +1,21 @@
 import * as cheerio from 'cheerio'
+import { marked } from 'marked'
 import path from 'node:path'
 import globule from 'globule'
-import superagent from 'superagent'
+
 
 import { JobData } from '../jobData.js'
-import { exists, readFile, writeFile } from '../utils.js'
+import { readFile, writeFile } from '../utils.js'
 import { GITEA_URL } from '../env.js'
+
+const MARKED_OPTS: marked.MarkedOptions = {
+  async: true,
+  gfm: true,
+  breaks: true,
+  headerIds: true,
+  silent: true,
+  smartLists: true,
+}
 
 async function processReadme(
   job,
@@ -15,10 +25,10 @@ async function processReadme(
 
   job.updateProgress({ status: 'in_progress', file: readmePath })
 
-  if (await exists(readmePath)) {
-    job.updateProgress({ status: 'done', file: readmePath })
-    return readFile(readmePath, 'utf-8')
-  }
+  // if (await exists(readmePath)) {
+  //   job.updateProgress({ status: 'done', file: readmePath })
+  //   return readFile(readmePath, 'utf-8')
+  // }
 
   let readmeInputPath
 
@@ -38,9 +48,18 @@ async function processReadme(
   }
 
   const rawMarkdown = await readFile(readmeInputPath, { encoding: 'utf8' })
-  const readmeAsHTML = await renderMarkdown(rawMarkdown)
+  // const readmeAsHTML = await renderMarkdown(rawMarkdown)
 
-  const processedReadmeHTML = postProcessMarkdown(readmeAsHTML, ownerName, repoName)
+  const readmeFolder = path.dirname(path.relative(inputDir, readmeInputPath))
+  const readmeAsHTML = await renderMarkdown(rawMarkdown, ownerName, repoName, readmeFolder)
+
+  const processedReadmeHTML = readmeAsHTML
+  // const processedReadmeHTML = postProcessMarkdown(
+  //   readmeAsHTML,
+  //   ownerName,
+  //   repoName,
+  //   readmeFolder,
+  // )
 
   await writeFile(readmePath, processedReadmeHTML)
     .then(() => job.updateProgress({ status: 'done', file: readmePath }))
@@ -66,28 +85,65 @@ function findReadmeFile(inputDir) {
   return null
 }
 
-async function renderMarkdown(rawMarkdown) {
-  const giteaMarkdownEndpoint = 'http://gitea:3000/api/v1/markdown/raw'
+const Renderer = new marked.Renderer(MARKED_OPTS)
 
-  const res = await superagent
-    .post(giteaMarkdownEndpoint)
-    .set('Content-Type', 'application/json')
-    .send(rawMarkdown)
+function ImageRenderer(baseUrl: string, readmeFolder: string) {
+  return (href: string, title: string, text: string) => {
+    const isRelativeUri = !href.match(/^https?:\/\//)
+    if (isRelativeUri) {
+      if (href.startsWith('/')) {
+        href = href.slice(1)
+      } else {
+        href = path.join(readmeFolder, href)
+      }
+      href = `${baseUrl}/${href}`
+    } else {
+      href = toRawUrl(href)
+    }
 
-  return res.text
+    return `<img src="${href}" loading="lazy" data-cy="relative-readme-img" alt="${text}" title="${title}">`
+  }
+}
+
+async function renderMarkdown(rawMarkdown: string, ownerName: string, repoName: string, readmeFolder: string) {
+  const baseUrl = `${GITEA_URL}/${ownerName}/${repoName}/raw`
+
+  Renderer.image = ImageRenderer(baseUrl, readmeFolder)
+
+  return marked(rawMarkdown, { renderer: Renderer })
+}
+
+function toRawUrl(url: string) {
+
+  const hostName = new URL(url).hostname
+  switch (hostName) {
+    case 'github.com':
+      url = url
+        .replace('github.com', 'raw.githubusercontent.com')
+        .replace('/blob', '')
+      break
+    default:
+      break
+  }
+  return url
 }
 
 /**
  * Make urls absolute not relative
  */
-function postProcessMarkdown(readmeAsHtml, ownerName, repoName) {
+function postProcessMarkdown(readmeAsHtml: string, ownerName: string, repoName: string, readmeFolder: string) {
   const $ = cheerio.load(readmeAsHtml)
   $('img').each((_, elem) => {
     const img = $(elem)
-    const src = img.attr('src')
+    let src = img.attr('src')
 
     const isRelativeUri = !src.match(/^https?:\/\//)
     if (isRelativeUri) {
+      if (src.startsWith('/')) {
+        src = src.slice(1)
+      } else {
+        src = path.join(readmeFolder, src)
+      }
       const rawUrl = `${GITEA_URL}/${ownerName}/${repoName}/raw/${src}`
       img.attr('src', rawUrl)
       img.attr('data-cy', 'relative-readme-img')
