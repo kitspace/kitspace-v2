b558f71310a60a7812008fcd845fdaa9e93e4c9d
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-22 10:52:22 +0100

    Add script for testing processor

---
+++ scripts/clear_volumes_and_test_processor.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+function concatenate_args
+{
+    string=""
+    for a in "$@" # Loop over arguments
+    do
+        if [[ "$string" != "" ]]
+        then
+            string+=" " # Delimeter
+        fi
+        string+="$a"
+    done
+    echo "$string"
+}
+
+# clear docker volumes
+docker-compose down -v
+
+# you can pass arguments to mocha e.g. `-g multi`
+args="$(concatenate_args "$@")"
+docker-compose run processor sh -c "yarn test ${args}"
