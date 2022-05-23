298ae3c366f0fc5e5f4bd148b54d2780702274c1
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-05-23 18:07:35 +0200

    fixup! e2e: add commit info when running inside docker

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -8,8 +8,8 @@ if [[ -z $GITHUB_TOKEN ]]; then
 else
     # The tests are running in CI.
     npx wait-on http://kitspace.test:3000 && cypress run --parallel --record -k $CYPRESS_RECORD_KEY \
-        -b $browser --group $group --ci-build-id $COMMIT_INFO_SHA \
-        -e COMMIT_INFO_BRANCH=${COMMIT_INFO_BRANCH##*/} \
+        -b $browser --group $group --ci-build-id $GITHUB_TOKEN \
+        -e COMMIT_INFO_BRANCH=${COMMIT_INFO_BRANCH} \
         -e COMMIT_INFO_MESSAGE=$COMMIT_INFO_MESSAGE \
         -e COMMIT_INFO_EMAIL=$COMMIT_INFO_EMAIL \
         -e COMMIT_INFO_AUTHOR=$COMMIT_INFO_AUTHOR \
