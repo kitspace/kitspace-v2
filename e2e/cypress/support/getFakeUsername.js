f78dab51ce93c55754646d7eb290dfdf69367a1f
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-24 13:20:19 +0100

    Use faker.internet.userName in e2e
    fixes #356

--- e2e/cypress/support/getFakeUsername.js
+++ e2e/cypress/support/getFakeUsername.js
@@ -8,9 +8,9 @@ export const getFakeUsername = () => {
   let username
   try {
     // It might fail to find unique username in the specified `maxTime`
-    username = faker.unique(faker.name.firstName, undefined, { maxTime: 50 })
+    username = faker.unique(faker.internet.userName, undefined, { maxTime: 50 })
   } catch (e) {
-    username = faker.name.firstName()
+    username = faker.internet.userName()
   }
   // This will make probability of returning the same username 1/1000 of the `faker.unique`.
   const suffix = (Math.random() * 1_000).toFixed()
