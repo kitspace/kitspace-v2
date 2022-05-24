1b2ff70c5a6320a27d2b2a20201a69a51a99c88e
Kaspar Emanuel <kaspar@kitspace.org>
2022-05-24 21:57:11 +0100

    processor: migrate to tsc
    - Remove redundant `tsc` and `start` scripts in processor.
    - Avoid using `yarn` in the container `CMD`.
      See, https://github.com/goldbergyoni/nodebestpractices#-82-bootstrap-using-node-command-avoid-npm-start
    - cp none ts assets `src` -> `dist/src`
    - Move setting test environment variables to docker command
    - Remove unused package (backoff)

--- scripts/clear_volumes_and_test_processor.sh
+++ scripts/clear_volumes_and_test_processor.sh
@@ -20,4 +20,4 @@ docker-compose down -v
 
 # you can pass arguments to mocha e.g. `-g multi`
 args="$(concatenate_args "$@")"
-docker-compose run processor sh -c "yarn test ${args}"
+docker-compose run -e LOG_LEVEL=debug -e DATA_DIR=/data/test processor sh -c "yarn install && yarn test ${args}"
