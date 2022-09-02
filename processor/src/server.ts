895b46644b0e1530d0d667d768acd37f131a0f8d
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-09-02 22:29:00 +0200

    Log the `LOG_LEVEL`

--- processor/src/server.ts
+++ processor/src/server.ts
@@ -3,13 +3,15 @@ import prexit from 'prexit'
 import { createApp } from './app'
 import { giteaDB } from './giteatDB'
 
-log.setDefaultLevel((process.env.LOG_LEVEL as log.LogLevelDesc) || log.levels.INFO)
+const LOG_LEVEL = (process.env.LOG_LEVEL as log.LogLevelDesc) || log.levels.INFO
+log.setDefaultLevel(LOG_LEVEL)
 
 const app = createApp('/gitea-data/git/repositories', { giteaDB })
 
 const port = 5000
 app.listen(port, () => {
   log.info(`processor listening on http://localhost:${port}`)
+  log.info('log level is set to ', LOG_LEVEL)
 })
 
 prexit(async () => {
