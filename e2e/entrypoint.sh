351989ba5bae80c54d057916c4aa2ebd482fc130
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-09-04 20:00:45 +0200

    CI: escape commit message when passing it to cypress

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -6,11 +6,13 @@ if [[ -z $GITHUB_TOKEN ]]; then
     # The tests are running locally.
     npx wait-on http://kitspace.test:3000 && cypress run -b chrome
 else
+    escaped_commit_info=$(python3 -c "import shlex; print(shlex.quote('''$COMMIT_INFO_MESSAGE'''))")
+    echo $escaped_commit_info
     # The tests are running in CI.
     npx wait-on http://kitspace.test:3000 && cypress run --parallel --record -k $CYPRESS_RECORD_KEY \
         -b $browser --group $group --ci-build-id $BUILD_ID \
         -e COMMIT_INFO_BRANCH=$COMMIT_INFO_BRANCH \
-        -e COMMIT_INFO_MESSAGE=$COMMIT_INFO_MESSAGE \
+        -e COMMIT_INFO_MESSAGE=$escaped_commit_info \
         -e COMMIT_INFO_EMAIL=$COMMIT_INFO_EMAIL \
         -e COMMIT_INFO_AUTHOR=$COMMIT_INFO_AUTHOR \
         -e COMMIT_INFO_SHA=$COMMIT_INFO_SHA \
