0ad1c6c9208bb61d3784f510a11009547dc24233
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-09-20 09:59:38 +0200

    processor: use `marked` for readme processing

--- processor/src/tasks/processReadme.ts
+++ processor/src/tasks/processReadme.ts
@@ -1,12 +1,18 @@
-import * as cheerio from 'cheerio'
 import { marked } from 'marked'
 import path from 'node:path'
 import globule from 'globule'
 
 
-import { JobData } from '../jobData.js'
-import { readFile, writeFile } from '../utils.js'
 import { GITEA_URL } from '../env.js'
+import {
+  exists,
+  isRelativeUrl,
+  normalizeRelativeUrl,
+  readFile,
+  toGitHubRawUrl,
+  writeFile
+} from '../utils.js'
+import { JobData } from '../jobData.js'
 
 const MARKED_OPTS: marked.MarkedOptions = {
   async: true,
@@ -19,18 +25,18 @@ const MARKED_OPTS: marked.MarkedOptions = {
 
 async function processReadme(
   job,
-  { inputDir, kitspaceYaml, outputDir, ownerName, repoName }: Partial<JobData>,
+  { inputDir, kitspaceYaml, outputDir, ownerName, repoName, originalUrl }: Partial<JobData>,
 ) {
   const readmePath = path.join(outputDir, 'readme.html')
 
   job.updateProgress({ status: 'in_progress', file: readmePath })
 
-  // if (await exists(readmePath)) {
-  //   job.updateProgress({ status: 'done', file: readmePath })
-  //   return readFile(readmePath, 'utf-8')
-  // }
+  if (await exists(readmePath)) {
+    job.updateProgress({ status: 'done', file: readmePath })
+    return readFile(readmePath, 'utf-8')
+  }
 
-  let readmeInputPath
+  let readmeInputPath: string
 
   if (kitspaceYaml.readme) {
     readmeInputPath = path.join(inputDir, kitspaceYaml.readme)
@@ -48,18 +54,15 @@ async function processReadme(
   }
 
   const rawMarkdown = await readFile(readmeInputPath, { encoding: 'utf8' })
-  // const readmeAsHTML = await renderMarkdown(rawMarkdown)
 
   const readmeFolder = path.dirname(path.relative(inputDir, readmeInputPath))
-  const readmeAsHTML = await renderMarkdown(rawMarkdown, ownerName, repoName, readmeFolder)
-
-  const processedReadmeHTML = readmeAsHTML
-  // const processedReadmeHTML = postProcessMarkdown(
-  //   readmeAsHTML,
-  //   ownerName,
-  //   repoName,
-  //   readmeFolder,
-  // )
+  const processedReadmeHTML = await renderMarkdown(
+    rawMarkdown,
+    ownerName,
+    repoName,
+    readmeFolder,
+    originalUrl
+  )
 
   await writeFile(readmePath, processedReadmeHTML)
     .then(() => job.updateProgress({ status: 'done', file: readmePath }))
@@ -70,7 +73,7 @@ async function processReadme(
   return processedReadmeHTML
 }
 
-function findReadmeFile(inputDir) {
+function findReadmeFile(inputDir: string) {
   const readmeFiles = globule.find(
     path.join(inputDir, 'readme?(.markdown|.mdown|.mkdn|.md|.rst)'),
     {
@@ -85,84 +88,46 @@ function findReadmeFile(inputDir) {
   return null
 }
 
-const Renderer = new marked.Renderer(MARKED_OPTS)
-
 function ImageRenderer(baseUrl: string, readmeFolder: string) {
   return (href: string, title: string, text: string) => {
-    const isRelativeUri = !href.match(/^https?:\/\//)
-    if (isRelativeUri) {
-      if (href.startsWith('/')) {
-        href = href.slice(1)
-      } else {
-        href = path.join(readmeFolder, href)
-      }
+    if (isRelativeUrl(href)) {
+      href = normalizeRelativeUrl(href, readmeFolder)
       href = `${baseUrl}/${href}`
     } else {
-      href = toRawUrl(href)
+      href = toGitHubRawUrl(href)
     }
 
-    return `<img src="${href}" loading="lazy" data-cy="relative-readme-img" alt="${text}" title="${title}">`
+    return `<img src="${href}" loading="lazy" data-cy="relative-readme-img" alt="${text}" title="${title || text}">`
   }
 }
 
-async function renderMarkdown(rawMarkdown: string, ownerName: string, repoName: string, readmeFolder: string) {
+
+function UrlRenderer(readmeFolder: string, originalUrl: string) {
+  return (href: string, title: string, text: string) => {
+    if (isRelativeUrl(href)) {
+      href = normalizeRelativeUrl(href, readmeFolder)
+      // the `/-/` is equivalent to `/HEAD/` but works for both GitHub and GitLab.
+      const rawUrl = `${originalUrl}/blob/-/${href}`
+      href = rawUrl
+    }
+    return `<a href=${href} target="_blank" rel="noopener noreferrer">${text}</a>`
+  }
+}
+
+
+async function renderMarkdown(
+  rawMarkdown: string,
+  ownerName: string,
+  repoName: string,
+  readmeFolder: string,
+  originalUrl: string) {
   const baseUrl = `${GITEA_URL}/${ownerName}/${repoName}/raw`
+  const Renderer = new marked.Renderer(MARKED_OPTS)
 
   Renderer.image = ImageRenderer(baseUrl, readmeFolder)
+  Renderer.link = UrlRenderer(readmeFolder, originalUrl)
 
   return marked(rawMarkdown, { renderer: Renderer })
-}
-
-function toRawUrl(url: string) {
-
-  const hostName = new URL(url).hostname
-  switch (hostName) {
-    case 'github.com':
-      url = url
-        .replace('github.com', 'raw.githubusercontent.com')
-        .replace('/blob', '')
-      break
-    default:
-      break
-  }
-  return url
-}
-
-/**
- * Make urls absolute not relative
- */
-function postProcessMarkdown(readmeAsHtml: string, ownerName: string, repoName: string, readmeFolder: string) {
-  const $ = cheerio.load(readmeAsHtml)
-  $('img').each((_, elem) => {
-    const img = $(elem)
-    let src = img.attr('src')
-
-    const isRelativeUri = !src.match(/^https?:\/\//)
-    if (isRelativeUri) {
-      if (src.startsWith('/')) {
-        src = src.slice(1)
-      } else {
-        src = path.join(readmeFolder, src)
-      }
-      const rawUrl = `${GITEA_URL}/${ownerName}/${repoName}/raw/${src}`
-      img.attr('src', rawUrl)
-      img.attr('data-cy', 'relative-readme-img')
-    }
-
-    // load readme images lazily
-    img.attr('loading', 'lazy')
-  })
-
-  $('a').each((_, elem) => {
-    const a = $(elem)
-    const href = a.attr('href')
-    if (href.startsWith('/')) {
-      const rawUrl = `${GITEA_URL}/${ownerName}/${repoName}/src${href}`
-      a.attr('href', rawUrl)
-    }
-  })
-
-  return $.html()
 }
 
 export default processReadme
