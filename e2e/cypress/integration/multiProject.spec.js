kitspace/kitspace-v2

commit 97b83df9a0d006bfdb32b559bf020fcd54e45712

UI|fix: show BOM even if there's no purchasable parts (#429)

frontend/src/components/Board/BuyParts/index.js
@@ -131,11 +131,13 @@ const BuyParts = ({ projectFullName, lines, parts }) => {
           <Segment attached className={styles.buttonSegment}>
             {retailerButtons}
           </Segment>
-          <Bom attached length={lines.length} parts={parts} tsv={linesToTsv()} />
           <DirectStores items={lines} multiplier={mult} />
         </>
       ) : (
-        <NoPurchasableParts />
+        <>
+          <NoPurchasableParts />
+          <Bom attached length={lines.length} parts={parts} tsv={linesToTsv()} />
+        </>
       )}
     </div>
   )

commit 3464ae8e58da528321becb814cf7fcd4576e3fa4

Fix: regression caused by #429

frontend/src/components/Board/BuyParts/Bom.js
@@ -21,7 +21,7 @@ const Bom = ({ length, parts, tsv }) => {
   }, [length])
 
   return (
-    <div className={styles.Bom}>
+    <div className={styles.Bom} data-cy="buy-parts-bom">
       <DoubleScrollBar>
         <TsvTable collapsed={collapsed} parts={parts} tsv={tsv} />
       </DoubleScrollBar>

frontend/src/components/Board/BuyParts/index.js
@@ -134,17 +134,20 @@ const BuyParts = ({ projectFullName, lines, parts }) => {
           <DirectStores items={lines} multiplier={mult} />
         </>
       ) : (
-        <>
-          <NoPurchasableParts />
-          <Bom attached length={lines.length} parts={parts} tsv={linesToTsv()} />
-        </>
+        <NoPurchasableParts />
       )}
+      <Bom attached length={lines.length} parts={parts} tsv={linesToTsv()} />
     </div>
   )
 }
 
 const NoPurchasableParts = () => (
-  <Segment attached disabled className={styles.buttonSegment}>
+  <Segment
+    attached
+    disabled
+    className={styles.buttonSegment}
+    data-cy="buy-parts-no-purchasable-parts"
+  >
     <p className={styles.NoPurchasablePartsText}>
       No parts to buy have been specified in this project&apos;s BOM yet.
     </p>

e2e/cypress/integration/multiProject.spec.js
@@ -238,10 +238,45 @@ describe('Multi project page', () => {
       'board-extra-menus',
       'order-pcb',
       'buy-parts',
+      'buy-parts-bom',
       'readme',
     ]
 
     pageComponents.forEach(c => {
+      cy.get(`[data-cy=${c}]`)
+    })
+  })
+
+  it('should render bom even if there is no purchasable parts', () => {
+    const username = getFakeUsername()
+    const email = faker.unique(faker.internet.email)
+    const password = '123456'
+    const syncedRepoUrlMultiProjects =
+      'https://github.com/kitspace-test-repos/glasgow'
+    const multiProjectsRepoName = 'glasgow'
+
+    cy.createUser(username, email, password)
+    cy.visit('/')
+    cy.get('[data-cy=user-menu]')
+
+    cy.forceVisit('/projects/new')
+
+    // Migrate the multiproject repo
+    cy.get('[data-cy=sync-field]').type(syncedRepoUrlMultiProjects)
+    cy.get('button').contains('Sync').click()
+
+    cy.url({ timeout: 60_000 }).should(
+      'contain',
+      `${username}/${multiProjectsRepoName}`,
+    )
+    // Wait for the repo to finish processing, by checking the visibility sub projects cards.
+    cy.get('[data-cy=project-card]', { timeout: 90_000 }).click({ force: true })
+    // wait for redirection
+    cy.get(['data-cy=sync-msg'], { timeout: 30_000 })
+
+    const buyPartsComponents = ['buy-parts-bom', 'buy-parts-no-purchasable-parts']
+
+    buyPartsComponents.forEach(c => {
       cy.get(`[data-cy=${c}]`)
     })
   })