47830b37fa65a903f70878765568501e4364d188
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-10-12 09:10:50 +0200

    e2e: should render :emoji: in readme and project description

--- e2e/cypress/integration/readme.spec.js
+++ e2e/cypress/integration/readme.spec.js
@@ -157,4 +157,32 @@ describe('Readme style', () => {
         cy.request($a[0].href).its('status').should('equal', 200)
       })
   })
+
+  it('should render :emoji: in readme and project description', () => {
+    const username = getFakeUsername()
+    const email = faker.unique(faker.internet.email)
+    const password = '123456'
+
+    const repoName = 'ogx360'
+    const syncedRepoUrl = 'https://github.com/kitspace-test-repos/ogx360'
+
+    cy.createUser(username, email, password)
+    cy.visit('/')
+    cy.get('[data-cy=user-menu]')
+
+    cy.forceVisit('/projects/new')
+
+    // Migrate the repo
+    cy.get('[data-cy=sync-field]').type(syncedRepoUrl)
+    cy.get('button').contains('Sync').click()
+
+    // Wait for redirection for project page
+    cy.url({ timeout: 60_000 }).should('contain', `${username}/${repoName}`)
+    // Wait for the repo to finish processing, by checking the visibility of info-bar.
+    cy.get('[data-cy=info-bar]', { timeout: 60_000 }).should('be.visible')
+
+    // the project description isAdd modern xinput USB support to your Original 📺 🎮
+    cy.get('[data-cy=project-description]').should('contain.text', '📺 🎮')
+    cy.get('[data-cy=readme]').should('contain.text', '🤓')
+  })
 })
