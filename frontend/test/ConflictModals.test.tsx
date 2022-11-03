262041708bdc9f38fa2f27deccc70ccfcf16d488
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-11-03 13:49:11 +0200

    test: add `opens conflict modal if the project name conflict with existing project`

--- frontend/test/ConflictModals.test.tsx
+++ frontend/test/ConflictModals.test.tsx
@@ -1,11 +1,16 @@
-import { afterEach, beforeAll, it, vi } from 'vitest'
-import { cleanup, render } from '@testing-library/react'
+import { afterEach, beforeAll, expect, it, vi } from 'vitest'
+import { cleanup, fireEvent, render } from '@testing-library/react'
 
-import { SyncConflictModal } from '@components/NewProject/ConflictModals'
+import Sync from '@components/NewProject/Sync'
 import AuthProvider from '@contexts/AuthContext'
-import GET_REPO from './fixtures/GetRepoRes.json'
+
+import GetRepo from './fixtures/GetRepoRes.json'
+
+const SESSION = { user: { username: 'tester' } }
 
 beforeAll(() => {
+  const GITEA_URL = 'https://gitea.kitspace.test'
+
   vi.mock('next/config', () => ({
     default: () => ({
       publicRuntimeConfig: {
@@ -17,24 +22,43 @@ beforeAll(() => {
   vi.mock('next/router', () => require('next-router-mock'))
 
   fetchMock.mockResponse(req => {
-    if (req.url === 'https://gitea.kitspace.test/api/v1/repos/tester/test_repo') {
-      return JSON.stringify(GET_REPO)
+    switch (req.url) {
+      case `${GITEA_URL}/api/v1/repos/tester/test_repo`:
+        return JSON.stringify(GetRepo)
+      case `${GITEA_URL}/api/v1/repos/migrate`:
+        return { status: 409 }
+      default:
+        throw new Error(`Unhandled request: ${req.url}`)
     }
   })
 })
 
-it('displays ConflictModal', () => {
-  render(
-    <AuthProvider initialSession={{ user: { username: 'tester' } }}>
-      <SyncConflictModal
-        conflictModalOpen={true}
-        originalProjectName="test_repo"
-        onClose={() => {}}
-        onDifferentName={() => {}}
-        onOverwrite={() => {}}
-      />
+it('opens conflict modal if the project name conflict with existing project', async () => {
+  const setUserOp = vi.fn()
+  const screen = render(
+    <AuthProvider initialSession={SESSION}>
+      <Sync setUserOp={setUserOp} />
     </AuthProvider>,
   )
+
+  // Write the url in the input
+  const input = screen.getAllByRole('textbox')
+  fireEvent.change(input[0], {
+    target: { value: 'https://github.com/kitspace-test-repos/test_repo' },
+  })
+
+  // Click the Sync button
+  fireEvent.click(screen.getByText('Sync'))
+  // Wait the modal to appear
+  await screen.findByText('Heads up!')
+  // It should set the userOp to sync
+  expect(setUserOp).toHaveBeenCalled()
+
+  // The conflict modal form should have the project name
+  const conflictModalInput = screen.getAllByRole('textbox')[1] as HTMLInputElement
+  expect(conflictModalInput.value).toBe('test_repo')
+  // The button should say overwrite because we haven't changed the project name yet.
+  expect(screen.getByText('Overwrite')).toBeTruthy()
 })
 
 afterEach(() => {
