9dd7fd91e7916fbce3ce6352979417b0c34b4857
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-11-15 00:40:53 +0200

    Fix: IBOM script loading and execution

--- frontend/src/components/Board/IBOM.jsx
+++ frontend/src/components/Board/IBOM.jsx
@@ -1,74 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import NextHead from 'next/head'
-import Script from 'next/script'
 import { string } from 'prop-types'
 import { Loader } from 'semantic-ui-react'
 
 import Page from '@components/Page'
 
 const IBOM = ({ projectFullname, html, pcbData, projectHref }) => {
-  const [ready, setReady] = useState(false)
+  const [IBOMScriptBody, setIBOMScriptBody] = useState(null)
+  const isReady = IBOMScriptBody != null
   const title = `${projectFullname} Kitspace Interactive Assembly Guide`
 
-  /*
-    i.   set the `pcbdata` var needed by IBOM
-    ii.  initialize IBOM
-    iii. make the title anchor tag linking to the project page.
-    iv.  prefetch project page.
-  */
-  const initScript = `
-    var pcbdata = ${pcbData};
-    document.getElementById('IBOM_script').addEventListener('load', () => {
-      window.onresize = resizeAll;
-      initBOM();
-      const pcbTitle = pcbdata.metadata.title;
-
-      document.querySelector('#title').innerHTML = "<a href=/${projectHref}>${projectFullname}</a>";
-      const head = document.head;
-      const link = document.createElement("link");
-      link.rel = "prefetch";
-      link.href = "${projectHref}";
-    });
-    `
-
   useEffect(() => {
-    const config = {
-      dark_mode: false,
-      show_pads: true,
-      show_fabrication: false,
-      show_silkscreen: true,
-      highlight_pin1: true,
-      redraw_on_drag: true,
-      board_rotation: 0,
-      checkboxes: 'Sourced,Placed',
-      bom_view: 'left-right',
-      layer_view: 'FB',
-      fields: ['Value', 'Footprint'],
-    }
-
-    window.config = config
+    //We need the text content of the script, the execution is handled by the `IBOMScriptsWrapper` component.
+    fetch('/static/IBOM/index.js')
+      .then(r => r.text())
+      .then(setIBOMScriptBody)
   }, [])
 
   return (
     <Page contentFullSize title={title}>
-      <Script
-        id="IBOM_script"
-        src="/static/IBOM/index.js"
-        onLoad={() => {
-          setReady(true)
-          return
-        }}
+      <IBOMScriptsWrapper
+        pcbData={pcbData}
+        projectFullname={projectFullname}
+        projectHref={projectHref}
+        scriptBody={IBOMScriptBody}
       />
-      <Script id="init_script">{initScript}</Script>
-      <NextHead>
-        {/* Styles for IBOM targets uses non pure selectors, e.g., `:root`, importing its style as
-         * a  module doesn't work. The other option was to add the style to `_app.scss`
-         * which will load on visiting any page.
-         */}
-        {/* eslint-disable-next-line @next/next/no-css-tags */}
-        <link href="/static/IBOM/index.css" rel="stylesheet" />
-      </NextHead>
-      {ready ? (
+      {isReady ? (
         <div
           className="ibom"
           // eslint-disable-next-line react/no-danger
@@ -81,10 +38,116 @@ const IBOM = ({ projectFullname, html, pcbData, projectHref }) => {
   )
 }
 
-IBOM.propTypes = {
+const config = {
+  dark_mode: false,
+  show_pads: true,
+  show_fabrication: false,
+  show_silkscreen: true,
+  highlight_pin1: true,
+  redraw_on_drag: true,
+  board_rotation: 0,
+  checkboxes: 'Sourced,Placed',
+  bom_view: 'left-right',
+  layer_view: 'FB',
+  fields: ['Value', 'Footprint'],
+}
+
+/*
+ * There are a couple of gotchas with the IBOM scripts:
+ ! 1. We can't use the `next/script` it only fetches and execute the script the first time it renders the <Script> component.
+ ! So navigating to another page and back will not execute the script again. This makes subsequent renders of the for other projects doesn't work too.
+ ! 2. Using the `next/head` component to add the script tag to the head, will actually load and execute the script.
+ ! *But* the ibom.js script has to many global variables that pollute the global namespace and cause conflicts. For example, visiting the ibom page
+ ! for any project will show the ibom for the very first project that was visited.
+
+ This wrapper component is a workaround for both of these issues.🔧
+ */
+const IBOMScriptsWrapper = ({
+  scriptBody,
+  pcbData,
+  projectHref,
+  projectFullname,
+}) => {
+  // To get around the global scope issue, we wrap the `ibom/index.js` script in a function and only set the required functions for user interactions
+  // in the window object. This way we can execute the script multiple times without any conflicts. See ibom script variable
+  const ibomScript = `
+  function ibom() {
+    ${scriptBody};
+    // Must-have globals for user interactions with the ibom.
+    return [
+      changeBomLayout,
+      changeBomMode,
+      changeCanvasLayout,
+      dnpOutline,
+      fabricationVisible,
+      initBOM,
+      loadSettings,
+      padsVisible,
+      referencesVisible,
+      saveImage,
+      saveSettings,
+      setBoardRotation,
+      setBomCheckboxes,
+      setDarkMode,
+      setFullscreen,
+      setHighlightPin1,
+      setRedrawOnDrag,
+      silkscreenVisible,
+      tracksVisible,
+      validateSaveImgDimension,
+      valuesVisible,
+      zonesVisible,
+    ]}
+  `
+
+  const initScript = `(function() {
+    // i. set the 'pcbdata' var needed by IBOM.
+    window.pcbdata = ${pcbData};
+    // ii. set the 'config' var needed by IBOM.
+    window.config = ${JSON.stringify(config)}
+    // iii. initialize IBOM.
+    // Must-have globals for user interactions with the ibom.
+    ibom().forEach(f => window[f.name] = f);
+    window.initBOM();
+    // iv. make the title anchor tag linking to the project page.
+    document.querySelector('#title').innerHTML = "<a href=/${projectHref}>${projectFullname}</a>";
+    // vi. prefetch project page.
+    const link = document.createElement("link");
+    link.rel = "prefetch";
+    link.href = "${projectHref}";
+    })()
+    `
+  return (
+    <NextHead>
+      {/* Styles for IBOM targets uses non pure selectors, e.g., `:root`, importing its style as
+       * a  module doesn't work. The other option was to add the style to `_app.scss`
+       * which will load on visiting any page.
+       */}
+      {/* eslint-disable-next-line @next/next/no-css-tags */}
+      <link href="/static/IBOM/index.css" rel="stylesheet" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: ibomScript,
+        }}
+      ></script>
+      {scriptBody != null && (
+        <script dangerouslySetInnerHTML={{ __html: initScript }}></script>
+      )}
+    </NextHead>
+  )
+}
+
+IBOMScriptsWrapper.propTypes = {
+  pcbData: string.isRequired,
   projectFullname: string.isRequired,
+  projectHref: string.isRequired,
+  scriptBody: string.isRequired,
+}
+
+IBOM.propTypes = {
   html: string.isRequired,
   pcbData: string.isRequired,
+  projectFullname: string.isRequired,
   projectHref: string.isRequired,
 }
 
