cc3df90da5b7d9b756b69b3e27041509c185faab
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-11-26 15:16:34 +0200

    UI: switch to `top-large.png` for card thumbnail

--- frontend/src/hooks/useThumbnail.js
+++ frontend/src/hooks/useThumbnail.js
@@ -6,7 +6,7 @@ const assetUrl = getConfig().publicRuntimeConfig.KITSPACE_ASSET_URL
 const fetcher = (...args) => fetch(...args, { method: 'HEAD' }).then(r => r.ok)
 
 const useThumbnail = (repoFullName, projectName) => {
-  const img = `${repoFullName}/HEAD/${projectName}/images/top.png`
+  const img = `${repoFullName}/HEAD/${projectName}/images/top-large.png`
   const src = `${assetUrl}/files/${img}`
   const { data, error } = useSWR(src, fetcher)
   return {
