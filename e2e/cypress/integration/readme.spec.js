5abdac16667e5eaa5bd046a34bc585b6c37ca3dd
Kaspar Emanuel <kaspar@kitspace.org>
2022-11-30 14:32:12 +0000

    Un-float promise in readme.spec.js

--- e2e/cypress/integration/readme.spec.js
+++ e2e/cypress/integration/readme.spec.js
@@ -30,15 +30,15 @@ describe('Relative README images URLs normalization', () => {
 
     cy.get('[data-cy=relative-readme-img]', { timeout: 60_000 })
       .should('have.attr', 'src')
-      .then(src => {
+      .then(src =>
         fetch(src).then(res => {
           assert(res.ok, 'expected "ok" http response when requesting image')
           assert(
             res.headers.get('content-type') === 'image/png',
             'expected http response to have content-type image/png',
           )
-        })
-      })
+        }),
+      )
   })
 
   it('handles readmes in folders', () => {
