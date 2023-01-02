7f597c734b84f2ec60e43ae6a6201a9f2c3126d0
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2023-01-02 16:42:25 +0200

    e2e: `Homepage search on mobile`

--- e2e/cypress/integration/search.spec.js
+++ e2e/cypress/integration/search.spec.js
@@ -1,17 +1,17 @@
 import { getFakeUser } from '../support/getFakeUser'
 
-describe('Homepage search bar', () => {
+describe('Navbar search', () => {
   it('should redirect to /search when search is submitted', () => {
     const queryTerm = 'awesome project'
 
     // Visit homepage
     cy.visit('/')
     // Write query term in the search field
-    cy.get('[data-cy=search-field] > input').type(queryTerm)
+    cy.get('#nav [data-cy=search-field] > input').type(queryTerm)
     // The URL shouldn't change before clicking on `Search`
     cy.url().should('equal', `${Cypress.config().baseUrl}/`)
     // Press enter
-    cy.get('[data-cy=search-form]').submit()
+    cy.get('#nav [data-cy=search-form]').submit()
     // Should redirect to the search page
     cy.url().should('include', `/search?q=${encodeURI(queryTerm)}`)
   })
@@ -41,16 +41,53 @@ describe('Homepage search bar', () => {
 
     cy.visit('/')
     // Write query term in the search field
-    cy.get('[data-cy=search-field] > input').type(repoName)
+    cy.get('#nav [data-cy=search-field] > input').type(repoName)
     // Press enter
-    cy.get('[data-cy=search-form]').submit()
+    cy.get('#nav [data-cy=search-form]').submit()
     // Should redirect to the search page
     cy.url().should('include', `/search?q=${encodeURI(repoName)}`)
     cy.get('[data-cy=project-card]').should('have.length.gte', 1)
   })
 })
 
-describe('Search', () => {
+describe('Homepage search on mobile', () => {
+  it('should display project card on submitting search form', () => {
+    const { username, email, password } = getFakeUser()
+
+    const repoName = 'CH330_Hardware'
+    const syncedRepoUrl = 'https://github.com/kitspace-test-repos/CH330_Hardware'
+
+    cy.createUser(username, email, password)
+    cy.visit('/')
+    cy.get('[data-cy=user-menu]')
+
+    cy.forceVisit('/projects/new')
+
+    // Migrate the repo
+    cy.get('[data-cy=sync-field] > input').type(syncedRepoUrl, { force: true })
+    cy.get('button').contains('Sync').click()
+
+    // Wait for redirection for project page
+    cy.url({ timeout: 60_000 }).should('contain', `${username}/${repoName}`)
+    // Wait for the repo to finish migration, by checking the visibility of processing-loader.
+    cy.get('[data-cy=processing-loader]', { timeout: 60_000 })
+    // Wait for the repo to finish processing, by checking the visibility of info-bar.
+    cy.get('[data-cy=info-bar]', { timeout: 60_000 }).should('be.visible')
+
+    // Convert to mobile view.
+    cy.visit('/')
+    cy.viewport('iphone-6')
+    // Write query term in the search field
+    cy.get('main [data-cy=search-field] > input').type(repoName)
+    // Press enter
+    cy.get('main [data-cy=search-form]').submit()
+    // Should redirect to the search page
+    cy.url().should('include', `/search?q=${encodeURI(repoName)}`)
+    cy.get('[data-cy=project-card]').should('have.length.gte', 1)
+  })
+})
+
+describe('/search route', () => {
   before(() => {
     /*
      * The purpose of this isn't actually visiting the homepage.
@@ -89,8 +126,8 @@ describe('Search', () => {
   it('should redirect to /search when search box is cleared', () => {
     cy.visit('search?q=query')
     // Clear the search form and press enter
-    cy.get('[data-cy=search-field] > input').clear()
-    cy.get('[data-cy=search-form]').submit()
+    cy.get('#nav [data-cy=search-field] > input').clear()
+    cy.get('#nav [data-cy=search-form]').submit()
     cy.url().should('equal', `${Cypress.config().baseUrl}/search?q=`)
   })
 })
