3fd3ec84b61dd153124edfa617ecc73e294e2e6f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2023-05-22 13:25:03 +0100

    Add more favicons (#576)
    * Add favicons in many more sizes
    
    * Add 48x and 96x icons

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -8,7 +8,6 @@ const Head = ({ title, description, url, ogImage }) => (
     <title>{title}</title>
     <meta content={description} name="description" />
     <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <link href="/static/favicon.png" rel="icon" type="image/png" />
     <meta content={url} property="og:url" />
     <meta content={title} property="og:title" />
     <meta content={description} property="og:description" />
@@ -18,21 +17,54 @@ const Head = ({ title, description, url, ogImage }) => (
     <meta content={ogImage} property="og:image" />
     <meta content="1200" property="og:image:width" />
     <meta content="630" property="og:image:height" />
+    <link
+      href="/icons/apple-touch-icon.png"
+      rel="apple-touch-icon"
+      sizes="180x180"
+    />
+    <link
+      href="/icons/favicon-96x96.png"
+      rel="icon"
+      sizes="96x96"
+      type="image/png"
+    />
+    <link
+      href="/icons/favicon-48x48.png"
+      rel="icon"
+      sizes="48x48"
+      type="image/png"
+    />
+    <link
+      href="/icons/favicon-32x32.png"
+      rel="icon"
+      sizes="32x32"
+      type="image/png"
+    />
+    <link
+      href="/icons/favicon-16x16.png"
+      rel="icon"
+      sizes="16x16"
+      type="image/png"
+    />
+    <link href="/site.webmanifest" rel="manifest" />
+    <link color="#373737" href="/icons/safari-pinned-tab.svg" rel="mask-icon" />
+    <meta content="#000000" name="msapplication-TileColor" />
+    <meta content="#000000" name="theme-color" />
   </NextHead>
 )
 
 Head.propTypes = {
-  title: string,
   description: string,
-  url: string,
   ogImage: string,
+  title: string,
+  url: string,
 }
 
 Head.defaultProps = {
-  title: 'Kitspace',
   description: 'A site for sharing electronics projects.',
-  url: 'https://kitspace.org',
   ogImage: 'https://kitspace.org/images/logo_meta.png',
+  title: 'Kitspace',
+  url: 'https://kitspace.org',
 }
 
 export default Head
