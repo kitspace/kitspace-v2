597fb4f18212dde4ef1e4e81bbadca882d98ddad
Ghanem <37152329+AbdulrhmnGhanem@users.noreply.github.com>
2023-07-11 15:58:29 +0300

    Add healthchek for `partinfo.kitspace.org` (#595)

---
+++ processor/src/healthChecks.ts
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+
+import { log } from './log.js'
+
+/**
+ * Check the health of the partinfo.kitspace.org by checking the graphql IDE
+ */
+export async function checkPartInfoHealth() {
+  const endpoint = 'https://partinfo.kitspace.org/graphql'
+  const res = await fetch(endpoint, {
+    headers: {
+      accept: 'text/html',
+    },
+  })
+  if (res.status !== 200) {
+    throw new Error(`${endpoint} health check failed with status ${res.status}`)
+  } else {
+    log.debug(`${endpoint} health check passed`)
+  }
+}
