4c93b04ec18c19c1d0f7e6296549d7f2e2a85ac4
Kaspar Emanuel <kaspar@kitspace.org>
2024-04-13 16:40:38 +0100

    Tweak search input icons etc.

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -10,37 +10,30 @@ import SearchFormModel from '@models/SearchFrom'
 import styles from './SearchInput.module.scss'
 
 const SearchInput = ({ className, isInstant }: SearchInputProps) => {
-  const [isLoading, setIsLoading] = useState(false)
   const { form, onChange, formatErrorPrompt, populate } = UseForm(SearchFormModel)
-  const { push } = useRouter()
+  const { push, replace } = useRouter()
   const { updateQuery, query } = useSearchQuery()
 
   useEffect(() => {
     populate({ query: query })
   }, [populate, query])
 
-  /**
-   * i.   Update the search query in the {@link SearchProvider}.
-   * ii.  Change the search query parameter `q`.
-   * iii. Make a `shallow` redirect to the new url `/search?q=${submitted query term}`.
-   ** note: the `shallow option only work if the path name doesn't change,
-   ** e.g., submitting a search from the `/search` path will be shallow,
-   ** from other paths it will actually redirect to `/search` which is the desired behavior.
-   ** see https://nextjs.org/docs/routing/shallow-routing#caveats.
-   */
   const handleSubmit = value => {
-    setIsLoading(true)
     updateQuery(value)
 
-    push(`/search?q=${encodeURIComponent(value)}`, undefined, {
-      shallow: true,
-    }).then(() =>
-      // When redirection finishes, replace the loading icon with the search one.
-      setIsLoading(false),
-    )
+    const path = value ? `/search?q=${encodeURIComponent(value)}` : '/'
+
+    if (isInstant) {
+      replace(path)
+    } else {
+      push(path)
+    }
   }
 
-  const debouncedSubmit = useDebounceCallback((event) => handleSubmit(event.target.value), 300)
+  const debouncedSubmit = useDebounceCallback(
+    event => handleSubmit(event.target.value),
+    300,
+  )
 
   const handleChange = (event, ...args) => {
     onChange(event, ...args)
@@ -66,7 +59,16 @@ const SearchInput = ({ className, isInstant }: SearchInputProps) => {
         onChange={handleChange}
         icon={
           isInstant ? (
-            <SearchIcon />
+            form.query ? (
+              {
+                style: { marginRight: 7 },
+                name: 'close',
+                link: true,
+                onClick: () => handleSubmit(''),
+              }
+            ) : (
+              <Icon className={styles.searchFieldIcon} name="search" />
+            )
           ) : (
             {
               name: 'search',
@@ -80,11 +82,6 @@ const SearchInput = ({ className, isInstant }: SearchInputProps) => {
     </Form>
   )
 }
-
-const LoadingIcon = () => (
-  <Icon loading className={styles.searchFieldIcon} name="spinner" />
-)
-const SearchIcon = () => <Icon className={styles.searchFieldIcon} name="search" />
 
 export default SearchInput
 
