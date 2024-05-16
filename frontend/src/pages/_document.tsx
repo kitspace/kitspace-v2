04e4e6912850c626bce2a09f27887a39707ab290
Kaspar Emanuel <kaspar@kitspace.org>
2024-05-16 12:26:45 +0100

    Add Lato to Google font loading

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -15,6 +15,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
