4f86620acb28bdda74bf76cfe4d5f43cf64bff0f
Kaspar Emanuel <kaspar@kitspace.org>
2024-09-22 15:47:28 +0100

    Revert "Switch "docker-compose" -> "docker compose""
    This reverts commit 5469afe24296be82a8f5560b432b257455d700f4.

--- scripts/install_gitea.sh
+++ scripts/install_gitea.sh
@@ -2,8 +2,8 @@
 
 set -Eeuo pipefail -o verbose
 
-docker compose -f docker-compose.yml -f docker-compose.deploy.yml up &
+docker-compose -f docker-compose.yml -f docker-compose.deploy.yml up &
 
 until docker logs kitspace_gitea_1 | grep 'ORM engine initialization successful' ; do sleep 3s; done
 
-docker compose stop
+docker-compose stop
