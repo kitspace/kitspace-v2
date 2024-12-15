74e54c888e7306c0100176bd4196a74e1dd81849
Kaspar Emanuel <kaspar@kitspace.org>
2024-12-15 14:43:58 +0000

    Re-add s3 config to terraform

---
+++ terraform/deployment/aws-s3.tf
@@ -0,0 +1,71 @@
+resource "aws_s3_bucket" "processor_bucket" {
+  bucket = "kitspace-staging-${var.branch_name}-4"
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.processor_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket     = aws_s3_bucket.processor_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "frontend_acl" {
+  bucket = aws_s3_bucket.processor_bucket.id
+  acl    = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.processor_bucket.id
+  versioning_configuration {
+    status = "Suspended"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.processor_bucket.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access,
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_acl.frontend_acl
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::kitspace-staging-${var.branch_name}-4/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_cors_configuration" "cors" {
+  bucket = aws_s3_bucket.processor_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
