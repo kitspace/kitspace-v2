c1e0770c17c5d9e89f7833094ff7c973eabc4622
Kaspar Emanuel <kaspar@kitspace.org>
2024-12-16 21:34:18 +0000

    Add more ec2 terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,43 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_key_pair" "info_kitspace_aws" {
+  key_name           = "info_kitspace_aws"
+  include_public_key = true
+}
+
+resource "aws_security_group" "kitspace_server" {
+  name = "kitspace-server"
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 // ---------------------------------------------------------
 // bunnynet
 
@@ -92,21 +129,25 @@ locals {
 }
 
 module "staging" {
-  for_each             = local.staging_branches
-  source               = "./deployment"
-  branch_name          = each.value
-  bunnynet_dns_zone_id = bunnynet_dns_zone.kitspace_dev_zone.id
-  domain               = "kitspace.dev"
+  for_each                          = local.staging_branches
+  source                            = "./deployment"
+  branch_name                       = each.value
+  bunnynet_dns_zone_id              = bunnynet_dns_zone.kitspace_dev_zone.id
+  domain                            = "kitspace.dev"
+  kitspace_server_security_group_id = aws_security_group.kitspace_server.id
+  ec2_instance_ssh_key_name         = data.aws_key_pair.info_kitspace_aws.key_name
   providers = {
     bunnynet = bunnynet.staging
   }
 }
 
 module "production" {
-  source               = "./deployment"
-  branch_name          = "production"
-  bunnynet_dns_zone_id = bunnynet_dns_zone.kitspace_org_zone.id
-  domain               = "kitspace.org"
+  source                            = "./deployment"
+  branch_name                       = "production"
+  bunnynet_dns_zone_id              = bunnynet_dns_zone.kitspace_org_zone.id
+  domain                            = "kitspace.org"
+  kitspace_server_security_group_id = aws_security_group.kitspace_server.id
+  ec2_instance_ssh_key_name         = data.aws_key_pair.info_kitspace_aws.key_name
   providers = {
     bunnynet = bunnynet.production
   }
